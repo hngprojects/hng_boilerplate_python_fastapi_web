b10f0d0b496a23d777b0e53a3d6a58b788a67b6d
NjengaC <victorcyrus01@gmail.com>
2023-10-31 16:20:52 +0300

    3 print number

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("\"Programming is like building a multilingual puzzle")
+print('"Programming is like building a multilingual puzzle')
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number:d} Battery street")
+print(f"{number} Battery street")
