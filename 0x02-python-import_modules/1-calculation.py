587d7c1f03b52c2f9db075770d2803da3311602b
NjengaC <victorcyrus01@gmail.com>
2023-11-02 19:38:47 +0300

    spaces

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1 as c
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, c.add(a,b)))
-    print("{} - {} = {}".format(a, b, c.sub(a,b)))
-    print("{} * {} = {}".format(a, b, c.mul(a,b)))
-    print("{} / {} = {}".format(a, b, c.div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
