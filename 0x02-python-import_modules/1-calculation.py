6747dc1404d4cd2892b3c4b379ba4d486a647e9e
NjengaC <victorcyrus01@gmail.com>
2023-11-02 19:33:55 +0300

    calling functions by ref

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, div, mul
+    import calculator_1 as c
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a,b)))
-    print("{} - {} = {}".format(a, b, sub(a,b)))
-    print("{} * {} = {}".format(a, b, mul(a,b)))
-    print("{} / {} = {}".format(a, b, div(a,b)))
+    print("{} + {} = {}".format(a, b, c.add(a,b)))
+    print("{} - {} = {}".format(a, b, c.sub(a,b)))
+    print("{} * {} = {}".format(a, b, c.mul(a,b)))
+    print("{} / {} = {}".format(a, b, c.div(a,b)))
