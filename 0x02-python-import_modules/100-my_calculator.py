6bb201a9d29c0a75279eefab40bfe84874e0d4d3
NjengaC <victorcyrus01@gmail.com>
2023-11-03 00:31:06 +0300

    pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
             print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        exit (1)
