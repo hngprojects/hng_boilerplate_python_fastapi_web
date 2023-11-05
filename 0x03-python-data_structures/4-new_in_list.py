3b971b3454bcfd0f9ffc576536c75a418483cb52
NjengaC <victorcyrus01@gmail.com>
2023-11-06 01:25:38 +0300

    'and' approach

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    if (idx < 0 or idx >= len(my_list)):
-        return(new_list)
-    else:
+    if (idx > 0 and idx < len(my_list)):
         new_list[idx] = element
-        return(new_list)
+    return(new_list)
