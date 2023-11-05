7f59b565c0fe31218346fb5dc913efd755f848bb
NjengaC <victorcyrus01@gmail.com>
2023-11-06 00:27:19 +0300

    return style

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
-        return(None)
+        return None
     else:
-        return(my_list[idx])
+        return (my_list[idx])
--- 0x03-python-data_structures/1-main.py
+++ 0x03-python-data_structures/1-main.py
@@ -2,6 +2,6 @@
 element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
+idx = 4
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
 
