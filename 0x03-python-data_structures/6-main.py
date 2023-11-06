636f3f569148770c6641571c7f86717084b1ed48
NjengaC <victorcyrus01@gmail.com>
2023-11-06 03:01:08 +0300

    tuple addition

---
+++ 0x03-python-data_structures/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
+
