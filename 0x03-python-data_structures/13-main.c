ae3c753908b24ae35d864c72f7597d27ce9bb042
NjengaC <victorcyrus01@gmail.com>
2023-11-07 01:47:18 +0300

    file names

---
+++ 0x03-python-data_structures/13-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *  * main - check the code for
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 50);
+	add_nodeint_end(&head, 972);
+	add_nodeint_end(&head, 17);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	return (0);
+}
