b33ec24ae26ca8cfccaf84d4f2deede949ea2f89
NjengaC <victorcyrus01@gmail.com>
2023-11-06 03:02:10 +0300

    spacers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)
     tuple_b += (0, 0)
 
-    new_tup = ((tuple_a[0] + tuple_b[0]),(tuple_a[1] + tuple_b[1]))
+    new_tup = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return (new_tup)
