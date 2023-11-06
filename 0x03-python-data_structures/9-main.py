44344796efe87d94ae1f431211c3a50dee2c678a
NjengaC <victorcyrus01@gmail.com>
2023-11-06 13:56:47 +0300

    max int in a tuple

---
+++ 0x03-python-data_structures/9-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+max_integer = __import__('9-max_integer').max_integer
+
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
