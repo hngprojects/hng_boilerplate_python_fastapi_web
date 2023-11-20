6d8d7b7c6c1df5252c6663fb41a80045958763bf
NjengaC <victorcyrus01@gmail.com>
2023-11-20 23:23:32 +0300

    lets see

---
+++ 0x05-python-exceptions/1-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
+
