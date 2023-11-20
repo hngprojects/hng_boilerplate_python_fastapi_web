dadbe0ed89ce02b1823b581e7ab73ae67002fb61
NjengaC <victorcyrus01@gmail.com>
2023-11-21 00:09:01 +0300

    needs the exact exception ZeroDivisionError:

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except Exception:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result:{}".format(result))
