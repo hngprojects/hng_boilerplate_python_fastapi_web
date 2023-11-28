50ff9935be0a3e62c7e7c5014e85bb7f45336f97
NjengaC <victorcyrus01@gmail.com>
2023-11-28 23:45:24 +0300

    everything is an object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" LockedClass
+"""
+
+
+class LockedClass:
+    __slots__= ['first_name']
