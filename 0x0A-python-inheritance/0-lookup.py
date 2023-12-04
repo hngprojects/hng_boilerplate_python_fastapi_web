6f1999a45890024f526970a4ed21bab137b6bebc
NjengaC <victorcyrus01@gmail.com>
2023-12-04 21:27:16 +0300

    dir module->returns a list of available attributes and meethods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""
+lookup - returns a kist of attributes
+
+"""
+def lookup(obj):
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/0-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+lookup = __import__('0-lookup').lookup
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print(lookup(MyClass2))
+print(lookup(int))
