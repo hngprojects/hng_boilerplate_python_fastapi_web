ff12ba54b9979594f01012230fa354bb88c94a85
NjengaC <victorcyrus01@gmail.com>
2023-12-05 19:06:14 +0300

    pycodestle:(

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,7 +6,6 @@ function writes to a file using json repr
 import json
 
 
-
 def save_to_json_file(my_obj, filename):
     """
     function write to a file using json repr
@@ -20,4 +19,3 @@ def save_to_json_file(my_obj, filename):
     """
     with open(filename, "w", encoding="utf-8") as files:
         json.dump(my_obj, files)
-
