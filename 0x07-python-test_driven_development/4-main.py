bf3821284db9f3adf94827e7f24abfc443e7cb07
NjengaC <victorcyrus01@gmail.com>
2023-12-09 16:20:26 +0300

    pycodestyle

---
+++ 0x07-python-test_driven_development/4-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+print_square = __import__('4-print_square').print_square
+
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,6 +5,7 @@ A MODULE TO PRINT A SQUARE
 
 """
 
+
 def print_square(size):
     """
     this isa function to print a square to
@@ -16,11 +17,10 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size <= 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
     if (size // 1) != size:
         raise TypeError("size must be an integer")
-
 
     for i in range(0, size):
         for j in range(0, size):
