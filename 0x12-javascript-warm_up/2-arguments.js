27ee4bfb7ba0c4ad01e270e412abb6ffd2301d1b
NjengaC <victorcyrus01@gmail.com>
2024-02-12 18:15:45 +0300

    arguments from the command line .JS

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
