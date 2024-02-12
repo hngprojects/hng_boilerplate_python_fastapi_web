d811dce647ec2ff8b220246e4d967711bbd953ca
NjengaC <victorcyrus01@gmail.com>
2024-02-12 22:46:26 +0300

    second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-let parg;
-let len;
+
 const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  parg = args.sort();
-  len = args.length;
-  console.log(parg[len - 2]);
+  const parg = args.map(Number).sort((a, b) => b - a);
+  console.log(parg[1]);
 }
