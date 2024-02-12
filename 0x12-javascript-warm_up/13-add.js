b3fac7b34e1ae082d169aafa831384d586db1f88
NjengaC <victorcyrus01@gmail.com>
2024-02-12 21:29:33 +0300

    Function exports

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+exports.add = function (a, b) {
+  a = parseInt(a);
+  b = parseInt(b);
+  return (a + b);
+};
---
+++ 0x12-javascript-warm_up/13-main.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const add = require('./13-add').add;
+console.log(add(3, 5));
