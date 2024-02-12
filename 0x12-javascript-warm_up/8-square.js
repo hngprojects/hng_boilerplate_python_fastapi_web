ffcc67abd3e0e39a0e9c20fcc0f4362e851e3c8e
NjengaC <victorcyrus01@gmail.com>
2024-02-12 19:19:10 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const num = parseInt(args[0]);
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    let line = '';
+    for (let j = 0; j < num; j++) {
+      line = line + 'X';
+    }
+    console.log(line);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
