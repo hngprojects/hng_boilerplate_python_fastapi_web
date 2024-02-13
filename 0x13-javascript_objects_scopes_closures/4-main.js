1a0bbba1b0a3608e81c3b61ab577a511f6adbe99
NjengaC <victorcyrus01@gmail.com>
2024-02-13 15:25:52 +0300

    new methods rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-main.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+const r1 = new Rectangle(2, 3);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
+r1.print();
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      let line = '';
+      for (let i = 0; i < this.width; i++) {
+        line = line + 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+};
