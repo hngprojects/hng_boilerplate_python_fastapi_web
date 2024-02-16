d5e349992b7773ed1869f199ff4b14b3ed88bef8
NjengaC <victorcyrus01@gmail.com>
2024-02-16 17:22:46 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+Module lists all states from the database given
+"""
 import MySQLdb
 import sys
 import sqlalchemy
+
+
 def list_states(username, password, database):
-    db = MySQLdb.connect(host='localhost', user=username, passwd='Njenga008!', db=database)
+    '''
+    Function connects to a server running on localhost
+    at port 3306 and selects all states and returns in asc order
+    '''
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd='Njenga008!', db=database)
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
@@ -15,6 +25,7 @@ def list_states(username, password, database):
 
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     username, password, database = sys.argv[1:]
