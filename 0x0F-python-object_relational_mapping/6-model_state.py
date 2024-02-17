592a50edc3bb441c0c05e81d2ffc2bf42e3e6032
NjengaC <victorcyrus01@gmail.com>
2024-02-17 13:46:52 +0300

    State model - inherits from Base

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
---
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -0,0 +1,4 @@
+-- Create database hbtn_0e_6_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
+USE hbtn_0e_6_usa;
+SHOW CREATE TABLE states;
