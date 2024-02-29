2d47a14db8988b9a9ca79e693b888f66c74c7ddb
NjengaC <victorcyrus01@gmail.com>
2024-02-29 18:49:33 +0300

    state

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Send request to the URL using curl, display only the status code
-#awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
-status_code=$(curl -sI "$1" | awk 'NR==1{print $2}')
-echo "$status_code"
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
