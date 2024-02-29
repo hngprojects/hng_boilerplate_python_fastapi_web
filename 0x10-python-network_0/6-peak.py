8f08809c817a6fc67269db77c010ae1c25ec7a5f
NjengaC <victorcyrus01@gmail.com>
2024-02-29 17:58:41 +0300

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,7 @@ def find_peak(list_of_integers):
 
     low, high = 0, len(list_of_integers) - 1
 
-    while low < higth:
+    while low < high:
         mid = (low + high) // 2
         if list_of_integers[mid] > list_of_integers[mid + 1]:
             high = mid
