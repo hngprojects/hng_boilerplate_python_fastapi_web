85013c102f1298f9480b3ff1c6fda7b2e7e986dd
NjengaC <victorcyrus01@gmail.com>
2024-03-02 00:06:31 +0300

    request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Module fetches https://alx-intranet.hbtn.io/status
-"""
 import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in a URL, sends a request to the URL and displays the value of the 
+takes in a URL, sends a request to the URL and displays the value of the
 X-Request-Id variable found in the header of the response.
 """
 import sys
