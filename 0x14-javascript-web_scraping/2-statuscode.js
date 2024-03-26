36f681b73409bcf8490e96c6af8fa6bd12bd6072
NjengaC <victorcyrus01@gmail.com>
2024-03-26 15:11:38 +0300

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const url = process.argv[2];
-
-if (url) {
-  request(url, (error, response, body) => {
-	  if (error) {
-      console.error(error);
-    } else {
-      console.log('code: ${response.statusCode}');
-    }
-  });
-} else {
-  process.exit(1);
-}
+request.get(process.argv[2]).on('response', function (response) {
+	  console.log(`code: ${response.statusCode}`);
+});
