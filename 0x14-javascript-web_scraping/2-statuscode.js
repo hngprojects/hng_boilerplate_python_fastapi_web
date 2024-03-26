a7deb041a639e61807178fa265bb8e282f76c415
NjengaC <victorcyrus01@gmail.com>
2024-03-26 16:41:33 +0300

    istandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
-	  console.log(`code: ${response.statusCode}`);
+  console.log(`code: ${response.statusCode}`);
 });
