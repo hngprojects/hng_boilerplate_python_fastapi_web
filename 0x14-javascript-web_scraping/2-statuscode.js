08335c39818944119eafb3c0ec0b96cbe5c3ebcc
NjengaC <victorcyrus01@gmail.com>
2024-03-26 13:50:09 +0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const request = require('request');
+
+const url = process.argv[2];
+
+if (url) {
+  request(url, (error, response, body) => {
+	  if (error) {
+      console.error(error);
+    } else {
+      console.log('code: ${response.statusCode}');
+    }
+  });
+} else {
+  process.exit(1);
+}
