ad6ff693120badbaa2dff28f9807d91b8fc3d161
NjengaC <victorcyrus01@gmail.com>
2024-03-26 12:58:29 +0300

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+if (filePath && contentToWrite) {
+  fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+} else {
+  process.exit(1);
+}
