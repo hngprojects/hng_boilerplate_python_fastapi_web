e730080f20915f62f3068dbd51a785d857cc9e95
zxenonx <xenon.nemrod@gmail.com>
2024-06-25 13:39:45 +0100

    Revert "template"

--- api/utils/settings.py
+++
@@ -1,4 +0,0 @@
-from decouple import config
-
-MONGO_URI = config("MONGO_URI")
-MONGO_DB_NAME = config("MONGO_DB_NAME")
--- setup.py
+++
@@ -1,2 +0,0 @@
-from setuptools import setup, find_packages
-setup(name='api', packages=find_packages())
