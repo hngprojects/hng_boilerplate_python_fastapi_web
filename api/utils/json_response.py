c5b88360b925013a9dbab658e29c640880bae796
Armolas <armolas06@gmail.com>
2024-07-19 23:53:20 +0100

    edited the JsonResponseDict to fit the enforced convention

--- api/utils/json_response.py
+++ api/utils/json_response.py
@@ -8,19 +8,13 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 
 
-class RequestStatus(Enum):
-    SUCCESS = "success"
-    ERROR = "error"
-
-
 class JsonResponseDict(JSONResponse):
 
-    def __init__(self, message: str, status: RequestStatus, data: dict | None = None, error: str = "", status_code=200):
+    def __init__(self, message: str, data: dict | None = None, error: str = "", status_code=200):
         """initialize your response"""
         self.message = message
         self.data = data
         self.error = error
-        self.status = status.value
         super().__init__(content=jsonable_encoder(self.response()), status_code=status_code)
 
     def __repr__(self):
@@ -28,7 +22,7 @@ class JsonResponseDict(JSONResponse):
             "message": self.message,
             "data": self.data,
             "error": self.error,
-            "status": self.status
+            "status_code": self.status_code
         }
 
     def __str__(self):
@@ -37,23 +31,23 @@ class JsonResponseDict(JSONResponse):
             "message": self.message,
             "data": self.data,
             "error": self.error,
-            "status": self.status
+            "status_code": self.status_code
         })
 
     def response(self):
         """return a json response dictionary"""
         print(f"response: {format(self)}")
-        if self.status == "success":
+        if self.status_code >= 300:
             return {
                 "message": self.message,
                 "data": self.data,
-                "status": self.status
+                "status_code": self.status_code
             }
         else:
             return {
                 "message": self.message,
                 "error": self.error,
-                "status": self.status
+                "status_code": self.status_code
             }
 
 """
@@ -61,7 +55,6 @@ usage:
 
 return JsonResponseDict(
             message="Job creation successful",
-            status=RequestStatus.SUCCESS,
             data={"job": new_job.to_dict()},
             status_code=status.HTTP_201_CREATED
         )
