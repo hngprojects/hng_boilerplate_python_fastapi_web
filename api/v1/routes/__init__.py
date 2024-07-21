4b88e1212fed9a88e2fc9e529780c9f1c75c7e4f
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-21 21:30:13 +0100

    updated the waitlist router to use the correct path

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from api.v1.routes.auth import auth
 from api.v1.routes.roles import role
 from api.v1.routes.newsletter_router import newsletter
-from api.v1.routes.waitlist import waitlist
+from api.v1.routes.waitlist import router as waitlist
 
 api_version_one = APIRouter(prefix="/api/v1")
 
