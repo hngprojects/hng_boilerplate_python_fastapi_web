0f5ac990a3b8b17395878c49a2c32558c51eb58b
Mohammed Mubarak <66879926+mmubarak0@users.noreply.github.com>
2024-07-22 01:50:11 +0200

    Revert "Feature to deactivate a user"

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -23,11 +23,5 @@ class Settings(BaseSettings):
     DB_NAME: str = config("DB_NAME")
     DB_TYPE: str = config("DB_TYPE")
 
-    MAIL_USERNAME: str = config("MAIL_USERNAME")
-    MAIL_PASSWORD: str = config('MAIL_PASSWORD')
-    MAIL_FROM: str = config('MAIL_FROM')
-    MAIL_PORT: int = config('MAIL_PORT')
-    MAIL_SERVER: str = config('MAIL_SERVER')
-
 
 settings = Settings()
