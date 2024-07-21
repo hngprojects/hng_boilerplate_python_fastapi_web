0f0fa87d2bd290c9fcbde541e5ef59b4872d1fee
Seyi Pythonian <prontomaster@gmail.com>
2024-07-21 05:42:02 +0100

    Update blog model with new fields and added blog creation to seed data

--- api/v1/models/blog.py
+++ api/v1/models/blog.py
@@ -1,32 +1,26 @@
 #!/usr/bin/env python3
-""" The Blog Post Model
-"""
-from sqlalchemy import (
-        Column,
-        Integer,
-        String,
-        Text,
-        Date,
-        ForeignKey,
-        DateTime,
-        func,
-        Boolean
-        )
+"""The Blog Post Model."""
+
+from sqlalchemy import Column, String, Text, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
-from datetime import datetime
 from api.v1.models.base import Base
 from api.v1.models.base_model import BaseModel
-from uuid_extensions import uuid7
 from sqlalchemy.dialects.postgresql import UUID, ARRAY
 
 
 class Blog(BaseModel, Base):
-    __tablename__ = 'blogs'
+    __tablename__ = "blogs"
 
-    author_id = Column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
+    author_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False,
+    )
     title = Column(String(100), nullable=False)
     content = Column(Text)
     image_url = Column(String(100), nullable=True)
     tags = Column(ARRAY(String(20)), nullable=True)
+    is_deleted = Column(Boolean, default=False, nullable=False)
+    excerpt = Column(String(500), nullable=True)
 
     author = relationship("User", backref="blogs")
