7da59a5f59c6ed05210413fc43a7d6ddaa6127e8
codeReaper0 <boluwatifetella@gmail.com>
2024-07-21 23:38:14 +0100

    fixed testing issues

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -4,6 +4,7 @@ from api.v1.routes.roles import role
 from api.v1.routes.blog import blogs
 from api.v1.routes.plans import plans
 from api.v1.routes.blogs import blogs
+from api.v1.routes.blog import blogs
 from api.v1.routes.newsletter_router import newsletter
 
 api_version_one = APIRouter(prefix="/api/v1")
