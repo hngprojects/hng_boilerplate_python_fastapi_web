2a1a97b18a973221d635ac943b08bdbc2ee1c0bf
donfortune <donfortunet.df@gmail.com>
2024-07-21 16:28:40 +0100

    added endpoints

--- api/db/database.py
+++ api/db/database.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 """ The database module
 """
 from sqlalchemy.ext.declarative import declarative_base
@@ -50,3 +50,65 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+#!/usr/bin/env python3
+# """ The database module
+# """
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from decouple import config
+# from contextlib import contextmanager
+# from api.v1.models.base import Base
+# from api.v1.models.user import User, WaitlistUser
+# from api.v1.models.org import Organization
+# from api.v1.models.profile import Profile
+# from api.v1.models.product import Product
+# from api.v1.models.subscription import Subscription
+# from api.v1.models.blog import Blog
+# from api.v1.models.job import Job
+
+# def get_db_engine():
+
+#     DB_TYPE = config("DB_TYPE", "postgresql")
+#     DB_NAME = config("DB_NAME", "hng_fast_api")
+#     DB_USER = config("DB_USER", "")
+#     DB_PASSWORD = config("DB_PASSWORD", "")
+#     DB_HOST = config("DB_HOST", "localhost")
+#     DB_PORT = config("DB_PORT", "5432")
+#     MYSQL_DRIVER = config("MYSQL_DRIVER", "")
+#     DATABASE_URL = ""
+
+#     if DB_TYPE == "mysql":
+#         DATABASE_URL = f'mysql+{MYSQL_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+#     elif DB_TYPE == "postgresql":
+#         DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+#     else:
+#         DATABASE_URL = "sqlite:///./database.db"
+
+#     if DB_TYPE == "sqlite":
+#         db_engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+#     else:
+#         db_engine = create_engine(DATABASE_URL, pool_size=32, max_overflow=64)
+    
+#     return db_engine
+
+# db_engine = get_db_engine()
+
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
+
+
+# def create_database():
+#     return Base.metadata.create_all(bind=db_engine)
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+# create_database()
+# db = next(get_db())
--- api/v1/routes/job.py
+++ api/v1/routes/job.py
@@ -39,3 +39,66 @@ async def create_job(
         "message": "Job listing created successfully",
         "data": new_job,
     }
+
+
+@job.get(
+    "",
+    response_model=dict[str, int | str | list[JobResponseSchema]],
+    status_code=status.HTTP_200_OK,
+    summary="Get all jobs",
+    description="This endpoint allows anyone to get all job listings.",
+)
+async def get_all_jobs(
+    db: Session = Depends(get_db),
+):
+    jobs = db.query(Job).all()
+    return {
+        "status": status.HTTP_200_OK,
+        "message": "Job listings retrieved successfully",
+        "data": jobs,   
+    }
+    
+
+
+
+
+
+# from fastapi import APIRouter, Depends, status
+# from sqlalchemy.orm import Session
+# from typing import List
+
+# from api.db.database import get_db
+# from api.v1.models.job import Job
+# from api.v1.schemas.job import JobCreate, JobResponseSchema
+
+# job = APIRouter(
+#     prefix="/api/v1/jobs",
+#     tags=["todos"],
+# )
+
+
+# @job.post(
+#     "",
+#     status_code=status.HTTP_201_CREATED,
+#     response_model=dict[str, int | str | JobResponseSchema],
+#     summary="Create job",
+#     description="This endpoint allows a user to create a new job by providing the necessary job details such as title, description, and requirements.",
+# )
+# async def create_job(
+#     job: JobCreate,
+#     db: Session = Depends(get_db),
+# ):
+#     new_job = Job(**job.model_dump(), user_id=current_user.id)
+
+#     # save new job
+#     db.add(new_job)
+#     db.commit()
+#     db.refresh(new_job)
+
+#     return {
+#         "status": status.HTTP_201_CREATED,
+#         "message": "Job listing created successfully",
+#         "data": new_job,
+#     }
+
+
