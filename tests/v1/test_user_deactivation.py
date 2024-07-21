b84aa80490840162c51fe7fbd7e7deffecc0d8e1
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-07-21 21:07:21 +0100

    Tests updated

--- tests/v1/test_user_deactivation.py
+++ tests/v1/test_user_deactivation.py
@@ -11,7 +11,7 @@ from api.v1.models.base import Base
 
 SQLALCHEMY_DATABASE_URL = config('DB_URL')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base.metadata.create_all(bind=engine)
