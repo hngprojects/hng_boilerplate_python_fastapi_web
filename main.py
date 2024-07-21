960be5659ce8ac0658832dde51ed5bfccf94201b
xaldovah <dxaldovah@gmail.com>
2024-07-21 20:21:57 +0300

    Resolved missing module issue in main

--- main.py
+++ main.py
@@ -36,9 +36,9 @@ app.add_middleware(
 
 app.add_exception_handler(CustomException, custom_exception_handler) # Newsletter custom exception registration
 
-app.include_router(auth)
+# app.include_router(auth)
 # app.include_router(users, tags=["Users"])
-#app.include_router(contact_us.router, tags=["Contact"])
+# app.include_router(contact_us.router, tags=["Contact"])
 
 app.include_router(api_version_one)
 
