456626fc12bc6ca73ba5507e6e870f9e6a92cba1
codeReaper0 <boluwatifetella@gmail.com>
2024-07-21 19:36:04 +0100

    Blog deletion endpoint added

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -3,6 +3,7 @@ from api.v1.routes.auth import auth
 from api.v1.routes.roles import role
 from api.v1.routes.blog import blogs
 from api.v1.routes.plans import plans
+from api.v1.routes.blogs import blogs
 from api.v1.routes.newsletter_router import newsletter
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -12,3 +13,4 @@ api_version_one.include_router(role)
 api_version_one.include_router(newsletter)
 api_version_one.include_router(blogs)
 api_version_one.include_router(plans)
+api_version_one.include_router(blogs)
--- tests/database.py
+++ tests/database.py
@@ -50,4 +50,3 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
-
