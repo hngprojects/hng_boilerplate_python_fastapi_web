c7d1540190b4b2f7df198950283b0c6173fcf5e5
Arnthorny <chidubem.anthony26@gmail.com>
2024-07-21 23:17:07 +0100

    chore: endpoint to deactivate invitation link

--- api/v1/routes/deactivate_invite_link_router.py
+++ api/v1/routes/deactivate_invite_link_router.py
@@ -12,36 +12,40 @@ from api.v1.schemas.auth import UserBase
 from api.v1.schemas.invitation import DeactivateInviteBody
 from api.utils.json_response import JsonResponseDict
 from uuid import UUID
-from datetime import datetime
+from datetime import datetime, timezone
 
 db = next(get_db())
+
 
 class CustomInviteDeactivateException(HTTPException):
     """
     Custom error handling
     """
+
     def __init__(self, detail: dict, status_code: int):
         super().__init__(detail=detail, status_code=status_code)
         self.message = detail.get("message")
         self.error = detail.get("error")
         self.status_code = status_code
 
-def custom_invite_deactivate_exception_handler(request: Request, exc: CustomInviteDeactivateException):
-    content={
-            "message": exc.message,
-            "error": exc.error,
-            "status_code": exc.status_code
-        }
-    print(content['status_code'])
+
+def custom_invite_deactivate_exception_handler(
+    request: Request, exc: CustomInviteDeactivateException
+):
+    content = {
+        "message": exc.message,
+        "error": exc.error,
+        "status_code": exc.status_code,
+    }
     return JsonResponseDict(**content)
 
 
-
 router = APIRouter()
+
+
 @router.patch(
     "/api/v1/invite/deactivate",
-    responses={400: {"message": "Validation error"},
-               403: {"message": "Forbidden"}}
+    responses={400: {"message": "Validation error"}, 403: {"message": "Forbidden"}},
 )
 def deactivate_invite_link(
     item: DeactivateInviteBody, user: UserBase = Depends(get_current_user)
@@ -49,49 +53,46 @@ def deactivate_invite_link(
     error_format: dict = {
         "message": "Validation error",
         "error": "",
-        "status_code": 400
+        "status_code": 400,
     }
     try:
-        link_data_arr: list = item.invitation_link.split('_')
+        link_data_arr: list = item.invitation_link.split("_")
         if len(link_data_arr) != 2:
             raise ValueError
         link_id = link_data_arr[1].strip()
-        if link_data_arr[0] != 'invite' or link_id == '':
+        if link_data_arr[0] != "invite" or link_id == "":
             raise ValueError
-        
+
         invitation = db.query(Invitation).where(Invitation.id == UUID(link_id)).first()
 
-        print(f'Link: {link_id}, {invitation.id}, {invitation.id}')
-    except(IndexError, ValueError):
+    except (IndexError, ValueError):
         invitation = None
-    
-    if not invitation or invitation.expires_at < datetime.now() or not invitation.is_valid:
-        error_format['error'] = "Invalid or expired invitation link"
-        raise CustomInviteDeactivateException(
-            status_code=400,
-            detail= error_format
-        )
+
+    if (
+        not invitation
+        or invitation.expires_at < datetime.now(timezone.utc)
+        or not invitation.is_valid
+    ):
+        error_format["error"] = "Invalid or expired invitation link"
+        raise CustomInviteDeactivateException(status_code=400, detail=error_format)
 
     org_id = invitation.organization_id
     org = db.query(Organization).where(Organization.id == org_id).first()
     if org is None:
-        error_format['error'] = "Organization not found"
+        error_format["error"] = "Organization not found"
         raise CustomInviteDeactivateException(
             status_code=400,
             detail=error_format,
         )
     if user.id != invitation.user_id:
-        error_format['message'] = "Forbidden"
-        error_format['error'] = "User is not authorized to deactivate this invitation link"
-        error_format['status_code'] = 403
-        raise CustomInviteDeactivateException(
-            status_code=403,
-            detail=error_format
+        error_format["message"] = "Forbidden"
+        error_format["error"] = (
+            "User is not authorized to deactivate this invitation link"
         )
+        error_format["status_code"] = 403
+        raise CustomInviteDeactivateException(status_code=403, detail=error_format)
     invitation.is_valid = False
     db.commit()
 
-    resp = {
-        "message": "Invitation link has been deactivated",
-        "status_code": 200}
+    resp = {"message": "Invitation link has been deactivated", "status_code": 200}
     return JsonResponseDict(**resp)
--- tests/v1/test_deactivate_invite_link_router.py
+++ tests/v1/test_deactivate_invite_link_router.py
@@ -1,4 +1,3 @@
-
 # Dependencies:
 # pip install pytest-mock
 # pip install pytest-asyncio
@@ -24,145 +23,163 @@ from api.v1.routes.deactivate_invite_link_router import CustomInviteDeactivateEx
 from api.v1.models.org import Organization
 
 
-
 from api.v1.schemas.auth import UserBase
 from api.v1.schemas.invitation import DeactivateInviteBody
 from uuid_extensions import uuid7
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 
 @pytest.fixture
 def setups():
     data = {}
 
-    data['user_id'] = uuid7()
-    data['inv_id'] = uuid7()
-    data['org_id'] = uuid7()
-    data['mock_invite'] = Invitation(
-        id=data['inv_id'], user_id=data['user_id'], organization_id=data['org_id'], is_valid=True
-        )
-        
-    data['mock_query'] = MagicMock()
-    data['mock_where'] = MagicMock()
+    data["user_id"] = uuid7()
+    data["inv_id"] = uuid7()
+    data["org_id"] = uuid7()
+    data["mock_invite"] = Invitation(
+        id=data["inv_id"],
+        user_id=data["user_id"],
+        organization_id=data["org_id"],
+        is_valid=True,
+    )
 
-    data['pyd_user'] = UserBase(
-        id=data['user_id'],
+    data["mock_query"] = MagicMock()
+    data["mock_where"] = MagicMock()
+
+    data["pyd_user"] = UserBase(
+        id=data["user_id"],
         first_name="Bob",
         last_name="Lash",
         email="bob@example.com",
-        created_at = datetime.now()
-        )
+        created_at=datetime.now(),
+    )
 
     yield data
 
     data.clear()
 
 
-
 class TestCodeUnderTest:
 
     # Deactivating a valid invitation link with correct user authorization
-    @patch('api.v1.routes.deactivate_invite_link_router.db')
+    @patch("api.v1.routes.deactivate_invite_link_router.db")
     def test_deactivate_valid_invitation_link(self, mocker, setups):
         mock_invite = Invitation(
-            id=setups['inv_id'], user_id=setups['user_id'], organization_id=setups['org_id'], is_valid=True,
-            expires_at=(datetime.now() + timedelta(hours=2)))
-    
-        mocker.query.return_value = setups['mock_query']
-        setups['mock_query'].where.return_value = setups['mock_where']
-        setups['mock_where'].first.return_value = mock_invite
+            id=setups["inv_id"],
+            user_id=setups["user_id"],
+            organization_id=setups["org_id"],
+            is_valid=True,
+            expires_at=(datetime.now(timezone.utc) + timedelta(hours=2)),
+        )
 
-        pyd_user = setups['pyd_user']
-        mocker.patch('api.utils.dependencies.get_current_user', return_value=pyd_user)
+        mocker.query.return_value = setups["mock_query"]
+        setups["mock_query"].where.return_value = setups["mock_where"]
+        setups["mock_where"].first.return_value = mock_invite
+
+        pyd_user = setups["pyd_user"]
+        mocker.patch("api.utils.dependencies.get_current_user", return_value=pyd_user)
 
         response = deactivate_invite_link(
             DeactivateInviteBody(invitation_link=f'invite_{str(setups["inv_id"])}'),
-            user=pyd_user
+            user=pyd_user,
         )
 
         assert response.status_code == 200
         assert response.message == "Invitation link has been deactivated"
 
     # Handling a valid invitation link and marking it as invalid
-    @patch('api.v1.routes.deactivate_invite_link_router.db')
+    @patch("api.v1.routes.deactivate_invite_link_router.db")
     def test_handle_valid_invitation_link_mark_invalid(self, mocker, setups):
 
-        mock_invite = Invitation(id=setups['inv_id'], user_id=setups['user_id'], organization_id=setups['org_id'], is_valid=True,
-        expires_at=(datetime.now() + timedelta(hours=2)))
-    
-        mocker.query.return_value = setups['mock_query']
-        setups['mock_query'].where.return_value = setups['mock_where']
-        setups['mock_where'].first.return_value = mock_invite
-
-        pyd_user = setups['pyd_user']
-        mocker.patch('api.utils.dependencies.get_current_user', return_value=pyd_user)
-        
-        response =  deactivate_invite_link(
-            DeactivateInviteBody(invitation_link=f'invite_{str(setups["inv_id"])}'),
-            user=pyd_user
+        mock_invite = Invitation(
+            id=setups["inv_id"],
+            user_id=setups["user_id"],
+            organization_id=setups["org_id"],
+            is_valid=True,
+            expires_at=(datetime.now(timezone.utc) + timedelta(hours=2)),
         )
-    
+
+        mocker.query.return_value = setups["mock_query"]
+        setups["mock_query"].where.return_value = setups["mock_where"]
+        setups["mock_where"].first.return_value = mock_invite
+
+        pyd_user = setups["pyd_user"]
+        mocker.patch("api.utils.dependencies.get_current_user", return_value=pyd_user)
+
+        response = deactivate_invite_link(
+            DeactivateInviteBody(invitation_link=f'invite_{str(setups["inv_id"])}'),
+            user=pyd_user,
+        )
+
         assert not mock_invite.is_valid
 
     # # Handling an invitation link with incorrect format
     # @pytest.mark.asyncio
     def test_handle_incorrect_format_invitation_link(self, mocker, setups):
-        
-        pyd_user = setups['pyd_user']
-        mocker.patch('api.utils.dependencies.get_current_user', return_value=pyd_user)
-    
+
+        pyd_user = setups["pyd_user"]
+        mocker.patch("api.utils.dependencies.get_current_user", return_value=pyd_user)
+
         with pytest.raises(CustomInviteDeactivateException) as exc_info:
             deactivate_invite_link(
-                DeactivateInviteBody(invitation_link='invalid_format'),
-                user=pyd_user
+                DeactivateInviteBody(invitation_link="invalid_format"), user=pyd_user
             )
-    
+
         assert exc_info.value.status_code == 400
-        assert exc_info.value.detail['error'] == "Invalid or expired invitation link"
+        assert exc_info.value.detail["error"] == "Invalid or expired invitation link"
 
     # # Handling a non-existent invitation link
-    @patch('api.v1.routes.deactivate_invite_link_router.db')
+    @patch("api.v1.routes.deactivate_invite_link_router.db")
     def test_handle_non_existent_invitation_link(self, mocker, setups):
 
-        mock_invite = Invitation(id=setups['inv_id'], user_id=setups['user_id'], organization_id=setups['org_id'], is_valid=True,
-        expires_at=(datetime.now() + timedelta(hours=2)))
-    
-        mocker.query.return_value = setups['mock_query']
-        setups['mock_query'].where.return_value = setups['mock_where']
-        setups['mock_where'].first.return_value = mock_invite
+        mock_invite = Invitation(
+            id=setups["inv_id"],
+            user_id=setups["user_id"],
+            organization_id=setups["org_id"],
+            is_valid=True,
+            expires_at=(datetime.now(timezone.utc) + timedelta(hours=2)),
+        )
 
-        pyd_user = setups['pyd_user']
-        mocker.patch('api.utils.dependencies.get_current_user', return_value=pyd_user)
-        
+        mocker.query.return_value = setups["mock_query"]
+        setups["mock_query"].where.return_value = setups["mock_where"]
+        setups["mock_where"].first.return_value = mock_invite
+
+        pyd_user = setups["pyd_user"]
+        mocker.patch("api.utils.dependencies.get_current_user", return_value=pyd_user)
+
         with pytest.raises(CustomInviteDeactivateException) as exc_info:
             deactivate_invite_link(
-                DeactivateInviteBody(invitation_link='invite_9999'),
-                user=pyd_user
+                DeactivateInviteBody(invitation_link="invite_9999"), user=pyd_user
             )
-    
+
         assert exc_info.value.status_code == 400
-        assert exc_info.value.detail['error'] == "Invalid or expired invitation link"
+        assert exc_info.value.detail["error"] == "Invalid or expired invitation link"
 
     # # Handling an expired or already deactivated invitation link
-    
-    @patch('api.v1.routes.deactivate_invite_link_router.db')
-    def test_handle_expired_or_deactivated_invitation_link(self, mocker, setups):
-        
-        mock_invite = Invitation(id=setups['inv_id'], user_id=setups['user_id'], organization_id=setups['org_id'], is_valid=True, expires_at=(datetime.now() - timedelta(hours=2)))
-    
-        mocker.query.return_value = setups['mock_query']
-        setups['mock_query'].where.return_value = setups['mock_where']
-        setups['mock_where'].first.return_value = mock_invite
 
-        pyd_user = setups['pyd_user']
-        mocker.patch('api.utils.dependencies.get_current_user', return_value=pyd_user)
-    
+    @patch("api.v1.routes.deactivate_invite_link_router.db")
+    def test_handle_expired_or_deactivated_invitation_link(self, mocker, setups):
+
+        mock_invite = Invitation(
+            id=setups["inv_id"],
+            user_id=setups["user_id"],
+            organization_id=setups["org_id"],
+            is_valid=True,
+            expires_at=(datetime.now(timezone.utc) - timedelta(hours=2)),
+        )
+
+        mocker.query.return_value = setups["mock_query"]
+        setups["mock_query"].where.return_value = setups["mock_where"]
+        setups["mock_where"].first.return_value = mock_invite
+
+        pyd_user = setups["pyd_user"]
+        mocker.patch("api.utils.dependencies.get_current_user", return_value=pyd_user)
 
         with pytest.raises(CustomInviteDeactivateException) as exc_info:
-         deactivate_invite_link(
-             DeactivateInviteBody(invitation_link=f'invite_{str(setups["inv_id"])}'),
-            user=pyd_user
-        )
-    
+            deactivate_invite_link(
+                DeactivateInviteBody(invitation_link=f'invite_{str(setups["inv_id"])}'),
+                user=pyd_user,
+            )
+
         assert exc_info.value.status_code == 400
-        assert exc_info.value.detail['error'] == "Invalid or expired invitation link"
+        assert exc_info.value.detail["error"] == "Invalid or expired invitation link"
