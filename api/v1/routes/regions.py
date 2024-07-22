bf97e6f40e1cfa7f9d66fbd69d64fdd9348d511f
Tomilola <83653154+Ayolola03@users.noreply.github.com>
2024-07-22 22:29:03 +0100

    final patches

--- api/v1/routes/regions.py
+++ api/v1/routes/regions.py
@@ -7,6 +7,10 @@ from api.v1.models.region import Region
 from api.db.database import get_db
 from api.utils.dependencies import get_current_user, get_current_admin
 from api.v1.models.user import User
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 region = APIRouter(
     prefix="/api/v1/regions",
@@ -58,10 +62,13 @@ def create_region(
 async def get_regions(
     db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
 ):
+    logger.info("Fetching all regions")
     try:
         regions = db.query(Region).all()
+        logger.info(f"Regions fetched successfully: {regions}")
         return regions
     except Exception as e:
+        logger.error(f"Unexpected error occurred: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An unexpected error occurred: " + str(e),
