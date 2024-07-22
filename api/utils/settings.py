0b1de1026744729257195559cf50e4584a0c76aa
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2024-07-22 09:30:31 +0100

    Fixed issue with settings

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
     DB_PASSWORD: str = config("DB_PASSWORD")
     DB_NAME: str = config("DB_NAME")
     DB_TYPE: str = config("DB_TYPE")
+    dev: str     = config("DB_TEST_ENV")
 
 
 settings = Settings()
