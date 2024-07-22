8f1abfe3bd0d12765bb31ee05d4a2b99d03f36ac
Dev-wonderful <adeonederful20@gmail.com>
2024-07-22 15:55:24 +0100

    fix: removed unnecessary imports

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import uvicorn
 from contextlib import asynccontextmanager
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.requests import Request
-from api.db.database import Base, engine
-
-from api.v1.routes.newsletter_router import newsletter
 from api.v1.routes.newsletter_router import (
     CustomException,
     custom_exception_handler
 )
+from api.v1.routes import api_version_one
+from api.utils.json_response import JsonResponseDict
 
-from api.v1.routes.auth import auth
-from api.v1.routes.user import user
-from api.v1.routes.roles import role
-
-Base.metadata.create_all(bind=engine)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -38,19 +31,17 @@ app.add_middleware(
 )
 
 app.add_exception_handler(CustomException, custom_exception_handler) # Newsletter custom exception registration
-app.include_router(newsletter, tags=["Newsletter"])
-
-app.include_router(auth)
-app.include_router(user)
+app.include_router(api_version_one)
 # app.include_router(users, tags=["Users"])
 
 
 @app.get("/", tags=["Home"])
 async def get_root(request: Request) -> dict:
-    return {
-        "message": "Welcome to API",
-        "URL": "",
-    }
+    return JsonResponseDict(
+        message="Welcome to API",
+        status_code=status.HTTP_200_OK,
+        data={"URL": ""}
+	)
 
 
 if __name__ == "__main__":
