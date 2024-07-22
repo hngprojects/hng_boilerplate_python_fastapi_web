b62d86ef94b6021a0ab3c40571147304c556fe2b
kumdan job <redeks123456@gmail.com>
2024-07-22 01:10:21 +0100

    added test for get job by id endpoint

--- api/v1/routes/roles.py
+++ api/v1/routes/roles.py
@@ -18,24 +18,33 @@ from api.utils.dependencies import get_current_admin
 
 role = APIRouter(prefix="/roles", tags=["Roles"])
 
+
 @role.post("/", response_model=ResponseModel, status_code=status.HTTP_201_CREATED)
-def create_role(current_admin: Annotated[User, Depends(get_current_admin)], role: RoleCreate, db: Session = Depends(get_db)):
+def create_role(
+    current_admin: Annotated[User, Depends(get_current_admin)],
+    role: RoleCreate,
+    db: Session = Depends(get_db),
+):
     db_role = db.query(Role).filter(Role.role_name == role.role_name).first()
     if db_role:
         raise HTTPException(status_code=400, message="Role already exists")
 
-    db_organization = db.query(Organization).filter(Organization.id == role.organization_id).first()
+    db_organization = (
+        db.query(Organization).filter(Organization.id == role.organization_id).first()
+    )
     if not db_organization:
         raise HTTPException(status_code=400, message="Organization does not exist")
 
-    permissions = db.query(Permission).filter(Permission.id.in_(role.permission_ids)).all()
+    permissions = (
+        db.query(Permission).filter(Permission.id.in_(role.permission_ids)).all()
+    )
     if len(permissions) != len(role.permission_ids):
         raise HTTPException(status_code=400, message="Some permissions do not exist")
 
     new_role = Role(
         role_name=role.role_name,
         organization_id=role.organization_id,
-        permissions=permissions
+        permissions=permissions,
     )
     db.add(new_role)
     db.commit()
