bb5cf9f5903e11b26d83ec8aa4b4106261ba818e
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2024-07-22 09:39:06 +0100

    Fixed issue with jsonResponse.

--- api/utils/json_response.py
+++ api/utils/json_response.py
@@ -15,7 +15,7 @@ class JsonResponseDict(JSONResponse):
         self.message = message
         self.data = data
         self.error = error
-        self.status_code = status_code  # Correctly set the status_code
+        self.status_code = status_code
         super().__init__(content=jsonable_encoder(self.response()), status_code=status_code)
 
     def __repr__(self):
