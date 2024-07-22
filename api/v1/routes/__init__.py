08b86ebb4024f02bc7a38a1082e9b4ced649401a
MikeSoft007 <mekpenyong2@gmail.com>
2024-07-22 10:23:50 +0200

    refactored invite endpoint

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -5,6 +5,7 @@ from api.v1.routes.plans import plans
 from api.v1.routes.newsletter_router import newsletter
 from api.v1.routes.invitation import invite
 
+
 api_version_one = APIRouter(prefix="/api/v1")
 
 api_version_one.include_router(auth)
