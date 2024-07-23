fe278ff9e51de9cd5000cb7e206b50881428919a
thugginnn <paul.iyiade4@gmail.com>
2024-07-23 21:35:20 +0100

    settings

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ class Settings(BaseSettings):
     DB_USER: str = config("DB_USER")
     DB_PASSWORD: str = config("DB_PASSWORD")
     DB_TYPE: str = config("DB_TYPE", default="postgresql")
+
+    MAIL_USERNAME: str = config("MAIL_USERNAME")
+    MAIL_PASSWORD: str = config("MAIL_PASSWORD")
+    MAIL_SERVER: str = config("MAIL_SERVER")
+    MAIL_PORT: int = config("MAIL_PORT", default=587)
+    MAIL_FROM: str = config("MAIL_FROM")
