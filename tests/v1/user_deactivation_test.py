deef7a62a3f44ace5f4791b2e92a2b3f727042f6
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-07-23 17:22:11 +0100

    fix: fix tests

--- tests/v1/user_deactivation_test.py
+++ tests/v1/user_deactivation_test.py
@@ -130,7 +130,7 @@ def test_user_inactive(test_db):
         "confirmation": True
     }, headers={'Authorization': f'Bearer {access_token}'})
 
-    assert user_already_deactivated.status_code == 400
+    assert user_already_deactivated.status_code == 403
     assert user_already_deactivated.json()['message'] == 'User is inactive'
 	
 
