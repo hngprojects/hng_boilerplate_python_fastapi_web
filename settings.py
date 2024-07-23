84964b1b632d09bde5fcc866f32b153760518be4
thugginnn <paul.iyiade4@gmail.com>
2024-07-23 21:09:54 +0100

    settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+from decouple import config
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    DB_HOST: str = config("DB_HOST")
+    DB_PORT: int = config("DB_PORT", default=5432)
+    DB_NAME: str = config("DB_NAME")
+    DB_USER: str = config("DB_USER")
+    DB_PASSWORD: str = config("DB_PASSWORD")
