fd0c6db340b43b5ac60f4c6d0416e35348d85e8f
SundayMba <sundinoh@gmail.com>
2024-07-24 08:52:34 +0100

    fix: seeding data failure

--- seed.py
+++ seed.py
@@ -2,17 +2,7 @@
 """ Populates the database with seed data
 """
 from api.db.database import create_database, get_db
-from api.v1.models.user import User, WaitlistUser
-from api.v1.models.org import Organization
-from api.v1.models.profile import Profile
-from api.v1.models.product import Product
-from api.v1.models.base import Base
-from api.v1.models.subscription import Subscription
-from api.v1.models.blog import Blog
-from api.v1.models.job import Job
-from api.v1.models.invitation import Invitation
-from api.v1.models.role import Role
-from api.v1.models.permission import Permission
+from api.v1.models import *
 
 # create_database()
 db = next(get_db())
@@ -27,7 +17,6 @@ org_1 = Organization(name= "Python Org", description="An organization for python
 org_2 = Organization(name="Django Org", description="An organization of django devs")
 org_3 = Organization(name="FastAPI Devs", description="An organization of Fast API devs")
 
-# user_role = Role()
 
 db.add_all([org_1, org_2, org_3])
 
@@ -35,9 +24,10 @@ db.add_all([org_1, org_2, org_3])
 org_1.users.extend([user_1, user_2, user_3])
 org_2.users.extend([user_1, user_3])
 org_3.users.extend([user_2, user_1])
+db.commit()
 
-product_1 = Product(name="bed", price=400000)
-product_2 = Product(name="shoe", price=150000)
+product_1 = Product(name="bed", price=400000, org_id=org_1.id)
+product_2 = Product(name="shoe", price=150000, org_id=org_2.id)
 
 profile_1 = Profile(bio='My name is John Doe', phone_number='09022112233')
 user_1.profile = profile_1
