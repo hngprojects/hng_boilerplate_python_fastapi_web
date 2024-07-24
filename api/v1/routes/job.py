11f52213da5f2f18d405bfa2364b50988286d285
Aniekan Akpan <aniekanbtc@gmail.com>
2024-07-24 21:16:11 +0100

    feat: endpoint to get details of job listings

--- api/v1/routes/job.py
+++ api/v1/routes/job.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from api.v1.schemas.job import JobCreate, JobResponse
 from api.v1.services.job import get_job_by_id, create_job
 from api.db.database import get_db
-from api.utils.dependencies import get_current_admin
+from api.utils.dependencies import get_current_user
 from api.v1.models.user import User
 from api.v1.models.job import Job
 
@@ -12,13 +12,13 @@ jobs = APIRouter(tags=["Jobs"])
 
 # Endpoint to create a new job
 @jobs.post("/jobs", response_model=JobResponse)
-def create_new_job(job_data: JobCreate, db: Session = Depends(get_db), user_id: str = Depends(get_current_admin)):
+def create_new_job(job_data: JobCreate, db: Annotated[Session, Depends(get_db)], user_id: Annotated[User, Depends(get_current_user)]):
     job = create_job(db, job_data, user_id)
     return job
 
 
 # Endpoint to retrieve job details by ID
 @jobs.get("/jobs/{job_id}", response_model=JobResponse)
-def get_job(job_id: int, db: Session = Depends(get_db), user_id: str = Depends(get_current_admin)):
+def get_job(job_id: str, db: Annotated[Session, Depends(get_db)], user_id: Annotated[User, Depends(get_current_user)]):
     job = get_job_by_id(db, job_id, user_id)
     return job
