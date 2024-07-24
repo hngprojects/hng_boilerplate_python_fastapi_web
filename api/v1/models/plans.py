90bbb523138e71cc6d14082cb6e70fe93df255c7
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-24 20:37:23 +0100

    Create plans.py
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

---
+++ api/v1/models/plans.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData
+
+metadata = MetaData()
+
+plans = Table(
+    "plans",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("plan_name", String, unique=True, index=True),
+    Column("amount", Integer),
+)
