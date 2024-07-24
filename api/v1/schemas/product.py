55f78031c6a55c7ac216fe076ced2cd89aac375d
okunola <abdayoade@gmail.com>
2024-07-24 22:43:02 +0100

    feat: created list organization products endpoint

--- api/v1/schemas/product.py
+++ api/v1/schemas/product.py
@@ -3,9 +3,19 @@ from typing import List
 
 class ProductBase(BaseModel):
     name: str
-    description: str
+    description: float
     price: float
 
+class ProductData(BaseModel):
+    current_page: int
+    total_pages: int
+    limit: int
+    total_items: int
+    products: List[ProductBase]
+
 class ProductList(BaseModel):
-    status_code: int
-    data: List[ProductBase]
+    status_code: int = 200
+    success: bool
+    message: str
+    data: ProductData
+    
