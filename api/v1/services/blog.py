2c068a3ec4f9c14e6c26a348c0989fcc8ec6bd40
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-25 00:14:56 +0100

    chore: cleaned up the services code

--- api/v1/services/blog.py
+++ api/v1/services/blog.py
@@ -1,26 +1,28 @@
-from typing import Optional
-from sqlalchemy.orm import Session
-from fastapi import HTTPException
-from api.v1.models.blog import Blog
 from api.v1.models.user import User
+from typing import Optional
+from typing import Any, Optional
+from sqlalchemy.orm import Session
+from api.core.base.services import Service
+from api.utils.db_validators import check_model_existence
+from api.v1.models.blog import Blog
+from uuid import UUID
+from fastapi import HTTPException
 
 
-class BlogService:
+class BlogService(Service):
     '''Blog service functionality'''
 
-    def __init__(self, db: Session):
-        self.db = db
+    def create(self, db: Session, schema):
+        '''Create a new blog post'''
 
-    def fetch(self, blog_id: str):
-        '''Fetch a blog post by its ID'''
-        blog_post = self.db.query(Blog).filter(Blog.id == blog_id).first()
-        if not blog_post:
-            raise HTTPException(status_code=404, detail="Post not Found")
-        return blog_post
+    def fetch_all(self, db: Session, **query_params: Optional[Any]):
+        '''Fetch all blog posts with option to search using query parameters'''
+
+    def fetch(self, db: Session, id: str):
+        return db.query(Blog).filter(Blog.id == id, Blog.is_deleted == False).first()
 
     def update(self, blog_id: str, title: Optional[str] = None, content: Optional[str] = None, current_user: User = None):
         '''Updates a blog post'''
-
         if not title or not content:
             raise HTTPException(
                 status_code=400, detail="Title and content cannot be empty")
@@ -44,3 +46,6 @@ class BlogService:
                 status_code=500, detail="An error occurred while updating the blog post")
 
         return blog_post
+
+    def delete(self, db: Session, id: int):
+        '''Deletes a blog post'''
