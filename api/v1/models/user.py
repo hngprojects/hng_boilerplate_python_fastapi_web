139183326a7271ddf6ef92b5c4667a701082eb9f
SundayMba <sundinoh@gmail.com>
2024-07-24 11:42:49 +0100

    fix: added is_verify and is_deleted field to the users table

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -32,6 +32,7 @@ class User(BaseTableModel):
     last_name = Column(String, nullable=True)
     is_active = Column(Boolean, default=True)
     is_super_admin = Column(Boolean, default=False)
+    is_deleted = Column(Boolean, default=False)
 
     profile = relationship("Profile", uselist=False, back_populates="user", cascade="all, delete-orphan")
     organizations = relationship("Organization", secondary=user_organization_association, back_populates="users")
