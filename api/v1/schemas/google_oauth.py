0fdf062e7ff16b2258b746839cac22bee1b57f7c
johnson-oragui <johnson.oragui@gmail.com>
2024-07-24 18:17:28 +0100

    added schemas for response after successful authentication

---
+++ api/v1/schemas/google_oauth.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class UserData(BaseModel):
+    """
+    Schema Response representing the validated google login
+    """
+    id: str
+    first_name: str
+    last_name: str
+    username: str
+    email: str
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
+
+class Tokens(BaseModel):
+    """
+    Schema representing tokens
+    """
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+class StatusResponse(BaseModel):
+    """
+    Schema Response to the end user
+    """
+    message: str
+    status: str
+    statusCode: int
+    tokens: Tokens
+    user: UserData
+
+
+
