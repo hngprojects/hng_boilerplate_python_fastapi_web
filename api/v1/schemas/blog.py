afb1bb3e3b8717b4104fbfc2175b2e1c0f43144f
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-24 15:55:53 +0100

    dded blog schema

--- api/v1/schemas/blog.py
+++ api/v1/schemas/blog.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from typing import List, Optional
 from uuid import UUID
 from datetime import datetime
+
 
 class BlogResponse(BaseModel):
     id: UUID
@@ -18,3 +19,22 @@ class BlogResponse(BaseModel):
     class Config:
         orm_mode = True
 
+
+class Blog(BaseModel):
+    id: UUID
+    author_id: str
+    title: str
+    content: str
+    image_url: Optional[List[HttpUrl]] = None
+    is_deleted: bool
+    excerpt: Optional[str] = None
+    tags: Optional[List[str]] = None
+    likes: Optional[int] = None
+    dislikes: Optional[int] = None
+    likes_audit: Optional[List[str]] = None
+    dislikes_audit: Optional[List[str]] = None
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
