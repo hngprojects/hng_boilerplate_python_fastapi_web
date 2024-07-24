cdd92597097925597e2c3b4559fa41a5e6b2cecb
SundayMba <sundinoh@gmail.com>
2024-07-24 08:06:49 +0100

    feat: added messages and modified users table and their relationship

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -47,6 +47,7 @@ class User(BaseTableModel, Base):
     blogs = relationship("Blog", back_populates="author", cascade="all, delete-orphan") 
     comments = relationship("Comment", back_populates="user", cascade="all, delete-orphan")
     invitations = relationship("Invitation", back_populates="user", cascade="all, delete-orphan")
+    messages = relationship("Message", back_populates="user", cascade="all, delete-orphan")
     
     def to_dict(self):
         obj_dict = super().to_dict()
