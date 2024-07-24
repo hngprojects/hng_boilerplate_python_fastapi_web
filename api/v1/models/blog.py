ee946f1abf90df2e756de5b87c9da5bd95732919
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-24 07:24:38 +0100

    added the updated fields to the blog table

--- api/v1/models/blog.py
+++ api/v1/models/blog.py
@@ -1,28 +1,25 @@
-#!/usr/bin/env python3
-"""The Blog Post Model."""
-
-from sqlalchemy import Column, String, Text, ForeignKey, Boolean
+from sqlalchemy import Column, String, Text, ForeignKey, Boolean, Integer, DateTime, ARRAY
 from sqlalchemy.orm import relationship
-# from api.v1.models.base import Base
-from api.v1.models.base_model import BaseTableModel
-from sqlalchemy.dialects.postgresql import UUID, ARRAY
-from uuid_extensions import uuid7
+from api.v1.models.base import Base
+from api.v1.models.base_model import BaseModel
+from datetime import datetime
 
+class Blog(BaseModel, Base):
+    __tablename = "blogs"
 
-class Blog(BaseTableModel):
-    __tablename__ = "blogs"
-
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
     author_id = Column(
-        String,
-        ForeignKey("users.id"),
-        nullable=False,
-        default=uuid7
+        String, 
+        ForeignKey("users.id"), 
+        nullable=False
     )
     title = Column(String(100), nullable=False)
-    content = Column(Text)
-    image_url = Column(String(100), nullable=True)
+    content = Column(Text, nullable=False)
+    image_url = Column(String(255), nullable=True)
     tags = Column(ARRAY(String(20)), nullable=True)
     is_deleted = Column(Boolean, default=False, nullable=False)
     excerpt = Column(String(500), nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow(), nullable=False)
 
     author = relationship("User", backref="blogs")
