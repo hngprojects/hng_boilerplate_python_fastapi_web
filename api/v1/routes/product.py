e5e97d64e3e3e73daf46b39bd2489f18f94b063b
Prideland-Okoi <prideland.okoi@gmail.com>
2024-07-24 14:54:55 +0100

    Create product.py - Add product retrieval endpoint in FastAPI
    - Define products APIRouter with tags
    - Implement endpoint to retrieve a product by org_id and pro_id
    - Validate existence of organization and product, return appropriate HTTP responses
    - Use ProductRequest for request validation and ProductResponse for response formatting
    
    Signed-off-by: Prideland-Okoi <prideland.okoi@gmail.com>

---
+++ api/v1/routes/product.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
+from sqlalchemy.orm import Session
+from api.v1.models.user import User
+from api.v1.models.organization import Organization
+from api.v1.models.product import Product
+from api.v1.schemas.product import ProductRequest, ProductResponse
+from api.db.database import get_db
+from api.utils.dependencies import get_current_user
+
+
+# db = next(get_db())
+
+
+products = APIRouter(tags=["products"])
+
+
+@app.post("/api/v1/products", response_model=ProductResponse)
+def get_product(request: ProductRequest, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    org_id = request.org_id
+    product_id = request.pro_id
+
+    # Check if the organization exists
+    organization = db.query(Organization).filter(
+        Organization.id == org_id).first()
+    if not organization:
+        raise HTTPException(status_code=404, detail="Organization not found")
+
+    # Check if the product exists within the organization
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    return {
+        "status_code": 200,
+        "message": f"Product {product.name} retrieved successfully",
+        "data": product
+    }
