8f3abf9caf1fdd6b8f3154f13183ca5ad32f27dd
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-24 20:38:26 +0100

    Create plans.py
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

---
+++ api/v1/routes/plans.py
@@ -0,0 +1,10 @@
+from sqlalchemy import insert
+from api.db import database
+from api.v1.models import plans
+from api.v1.schemas import plans
+
+async def create_plan(plan: PlanCreate):
+    query = insert(plans).values(plan_name=plan.plan_name, amount=plan.amount)
+    record_id = await database.execute(query)
+    return {**plan.dict(), "id": record_id}
+
