ddafae939e49b5d2d83e543def8269143f405437
SundayMba <sundinoh@gmail.com>
2024-07-24 13:29:39 +0100

    fix: changed get_current_admin to get_current-super_admin

--- tests/v1/user_deactivation_test.py
+++ tests/v1/user_deactivation_test.py
@@ -23,9 +23,9 @@ LOGIN_ENDPOINT = 'api/v1/auth/login'
 
 @pytest.fixture
 def mock_db_session():
-    """Fixture to create a mock database session."""
+    """Fixture to create a mock database session. api.v1.services.user.get_db"""
 
-    with patch("api.v1.services.user.get_db", autospec=True) as mock_get_db:
+    with patch("api.db.database.get_db", autospec=True) as mock_get_db:
         mock_db = MagicMock()
         # mock_get_db.return_value.__enter__.return_value = mock_db
         app.dependency_overrides[get_db] = lambda: mock_db
