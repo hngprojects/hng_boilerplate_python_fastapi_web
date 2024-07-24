f41cf9b8213204f13ed7e4c50fe1bf4375a5f8a4
Prideland-Okoi <prideland.okoi@gmail.com>
2024-07-24 15:00:05 +0100

    Update main.py - Include product routes in main FastAPI application
    - Add product router from api.v1.routes.roles
    - Integrate product-related endpoints into the main application
    
    
    Signed-off-by: Prideland-Okoi <prideland.okoi@gmail.com>

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from api.v1.routes.newsletter_router import (
 from api.v1.routes.auth import auth
 from api.v1.routes.user import user
 from api.v1.routes.roles import role
+from api.v1.routes.roles import product
 
 Base.metadata.create_all(bind=engine)
 
@@ -42,6 +43,7 @@ app.include_router(newsletter, tags=["Newsletter"])
 
 app.include_router(auth)
 app.include_router(user)
+app.include_router(product)
 # app.include_router(users, tags=["Users"])
 
 
