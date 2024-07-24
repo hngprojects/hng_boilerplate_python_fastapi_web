165dba1c64fe692d1432fe863cfa97fd6f385416
kumdan job <redeks123456@gmail.com>
2024-07-25 00:03:53 +0100

    fix: fixed new endpoint route and removed comments

--- api/v1/routes/user.py
+++ api/v1/routes/user.py
@@ -8,29 +8,39 @@ from api.utils.success_response import success_response
 from api.v1.models.user import User
 from api.v1.schemas.user import DeactivateUserSchema, UserBase, ChangePasswordSchema
 from api.db.database import get_db
-# from api.utils.dependencies import get_current_user
 from api.v1.services.user import user_service
 
 
-user = APIRouter(prefix='/users', tags=['Users'])
+user = APIRouter(prefix="/users", tags=["Users"])
 
-@user.get('/current-user', status_code=status.HTTP_200_OK, response_model=UserBase)
-def get_current_user_details(db: Session = Depends(get_db), current_user: User = Depends(user_service.get_current_user)):
-    '''Endpoint to get current user details'''
+
+@user.get("/current-user", status_code=status.HTTP_200_OK, response_model=UserBase)
+def get_current_user_details(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(user_service.get_current_user),
+):
+    """Endpoint to get current user details"""
 
     return current_user
 
 
-@user.post('/deactivation', status_code=status.HTTP_200_OK)
-async def deactivate_account(request: Request, schema: DeactivateUserSchema, db: Session = Depends(get_db), current_user: User = Depends(user_service.get_current_user)):
-    '''Endpoint to deactivate a user account'''
+@user.post("/deactivation", status_code=status.HTTP_200_OK)
+async def deactivate_account(
+    request: Request,
+    schema: DeactivateUserSchema,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(user_service.get_current_user),
+):
+    """Endpoint to deactivate a user account"""
 
-    reactivation_link = user_service.deactivate_user(request=request, db=db, schema=schema, user=current_user)
+    reactivation_link = user_service.deactivate_user(
+        request=request, db=db, schema=schema, user=current_user
+    )
 
     return success_response(
         status_code=200,
-        message='User deactivation successful',
-        data={'reactivation_link': reactivation_link}
+        message="User deactivation successful",
+        data={"reactivation_link": reactivation_link},
     )
 
 
@@ -47,31 +57,31 @@ async def deactivate_account(request: Request, schema: DeactivateUserSchema, db:
 #     )
 
 
-@user.get('/reactivation', status_code=200)
+@user.get("/reactivation", status_code=200)
 async def reactivate_account(request: Request, db: Session = Depends(get_db)):
-    '''Endpoint to reactivate a user account'''
+    """Endpoint to reactivate a user account"""
 
     # Get access token from query
-    token = request.query_params.get('token')
+    token = request.query_params.get("token")
 
     # reactivate user
     user_service.reactivate_user(db=db, token=token)
 
     return success_response(
         status_code=200,
-        message='User reactivation successful',
+        message="User reactivation successful",
     )
+
 
 @user.patch("/me/password", status_code=200)
 async def change_password(
-    # request: Request,
     schema: ChangePasswordSchema,
     db: Session = Depends(get_db),
     user: User = Depends(user_service.get_current_user),
 ):
     """Endpoint to change the user's password"""
 
-    user_service.change_password(schema.old_password,schema.new_password,user,db)
+    user_service.change_password(schema.old_password, schema.new_password, user, db)
 
     return {
         "status_code": 200,
