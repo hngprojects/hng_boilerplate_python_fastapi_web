ad2048bdf762c1f4cb8444ab182669c0eae02a14
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-24 19:56:19 +0100

    added the blog schema

--- api/v1/schemas/blog.py
+++ api/v1/schemas/blog.py
@@ -21,18 +21,14 @@ class BlogResponse(BaseModel):
 
 
 class Blog(BaseModel):
-    id: UUID
+ 
     author_id: str
     title: str
     content: str
-    image_url: Optional[List[HttpUrl]] = None
+    image_url: Optional[str]
     is_deleted: bool
-    excerpt: Optional[str] = None
-    tags: Optional[List[str]] = None
-    likes: Optional[int] = None
-    dislikes: Optional[int] = None
-    likes_audit: Optional[List[str]] = None
-    dislikes_audit: Optional[List[str]] = None
+    excerpt: Optional[str]
+    tags: [List[str]]
     created_at: datetime
     updated_at: datetime
 
