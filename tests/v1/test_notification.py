2ecb66a59fc3787a9bcb9d1052d03c8864f11765
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-07-24 16:49:38 +0100

    fix: Removed windows dependent import

--- tests/v1/test_notification.py
+++ tests/v1/test_notification.py
@@ -1,7 +1,3 @@
-import sys, os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
