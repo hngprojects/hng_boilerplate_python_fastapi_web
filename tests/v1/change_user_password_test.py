cb96c9071ee7c96e33f0973d62a0c08d8a7a0c00
kumdan job <redeks123456@gmail.com>
2024-07-24 13:27:07 +0100

    added extra tests

--- tests/v1/change_user_password_test.py
+++ tests/v1/change_user_password_test.py
@@ -1,80 +1,130 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from decouple import config
-import sys, os
+import os
+import sys
 import warnings
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
-from main import app
-from api.db.database import Base, get_db
-from api.v1.services.user import user_service
+from datetime import datetime, timezone
+from unittest.mock import MagicMock, patch
+
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+from uuid_extensions import uuid7
+
+from api.db.database import get_db
 from api.v1.models.user import User
-from api.v1.models.base import Base
-
-SQLALCHEMY_DATABASE_URL = config('DB_URL')
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.create_all(bind=engine)
-
-def override_get_db():
-	db = TestingSessionLocal()
-	try:
-		yield db
-	finally:
-		db.close()
-
-app.dependency_overrides[get_db] = override_get_db
+from api.v1.services.user import user_service
+from main import app
 
 client = TestClient(app)
+LOGIN_ENDPOINT = "api/v1/auth/login"
+CHANGE_PWD_ENDPOINT = "/api/v1/users/current-user/change-password"
 
-@pytest.fixture(scope="module")
-def test_db():
-	db = TestingSessionLocal()
-	yield db
-	db.close()
-	
 
-def create_user(test_db):
-	
-    # Add user to database
-    user = User(
-        username="randomuser1",
-        email="randomuser@gmail.com",
-        password=user_service.hash_password('randomPwd@123'),
-		first_name='Test',
-		last_name='User',
+@pytest.fixture
+def mock_db_session():
+    """Fixture to create a mock database session."""
+
+    with patch("api.v1.services.user.get_db", autospec=True) as mock_get_db:
+        mock_db = MagicMock()
+        # mock_get_db.return_value.__enter__.return_value = mock_db
+        app.dependency_overrides[get_db] = lambda: mock_db
+        yield mock_db
+    app.dependency_overrides = {}
+
+
+@pytest.fixture
+def mock_user_service():
+    """Fixture to create a mock user service."""
+
+    with patch("api.v1.services.user.user_service", autospec=True) as mock_service:
+        yield mock_service
+
+
+def create_mock_user(mock_user_service, mock_db_session):
+    """Create a mock user in the mock database session."""
+    mock_user = User(
+        id=str(uuid7()),
+        username="testuser",
+        email="testuser@gmail.com",
+        password=user_service.hash_password("Testpassword@123"),
+        first_name="Test",
+        last_name="User",
         is_active=True,
-        is_admin=False
+        # is_super_admin=False,
+        created_at=datetime.now(timezone.utc),
+        updated_at=datetime.now(timezone.utc),
     )
-    test_db.add(user)
-    test_db.commit()
-    test_db.refresh(user)
+    mock_db_session.query.return_value.filter.return_value.first.return_value = (
+        mock_user
+    )
+
+    # mock_db_session.return_value.__enter__.return_value = mock_user
+    # mock_user_service.hash_password.return_value = "hashed_password"
+    # mock_db_session.add.return_value = None
+    # mock_db_session.commit.return_value = None
+    # mock_db_session.refresh.return_value = None
+
+    return mock_user
 
 
+@pytest.mark.usefixtures("mock_db_session", "mock_user_service")
+def test_autheniticated_user(mock_db_session, mock_user_service):
 
-def test_user_password(test_db):
-	
-    create_user(test_db)
-	
-    login =  client.post('/api/v1/auth/login', data={
-        "username": "randomuser1",
-        "password": "randomPwd@123"
-    })
-    print(login)
-    access_token = login.json()['data']['access_token']
+    mock_user = create_mock_user(mock_user_service, mock_db_session)
 
-	
-    user_pwd_change = client.patch('/api/v1/users/current-user/change-password', json={
-        "old_password": "randomPwd@123",
-        "new_password": 'Ojobonandom@123'
-    }, headers={'Authorization': f'Bearer {access_token}'})
+    login = client.post(
+        LOGIN_ENDPOINT,
+        data={"username": "testuser", "password": "Testpassword@123"},
+    )
+    access_token = login.json()["data"]["access_token"]
+
+    user_pwd_change = client.patch(
+        CHANGE_PWD_ENDPOINT,
+        json={"old_password": "Testpassword@123", "new_password": "Ojobonandom@123"},
+    )
+    assert user_pwd_change.status_code == 401
+    assert user_pwd_change.json()["message"] == "Not authenticated"
+
+
+@pytest.mark.usefixtures("mock_db_session", "mock_user_service")
+def test_wrong_pwd(mock_db_session, mock_user_service):
+
+    mock_user = create_mock_user(mock_user_service, mock_db_session)
+
+    login = client.post(
+        LOGIN_ENDPOINT,
+        data={"username": "testuser", "password": "Testpassword@123"},
+    )
+    access_token = login.json()["data"]["access_token"]
+
+    user_pwd_change = client.patch(
+        CHANGE_PWD_ENDPOINT,
+        json={"old_password": "Testpassw23", "new_password": "Ojobonandom@123"},
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    assert user_pwd_change.status_code == 400
+    assert user_pwd_change.json()["message"] == "Incorrect old password"
+
+
+@pytest.mark.usefixtures("mock_db_session", "mock_user_service")
+def test_user_password(mock_db_session, mock_user_service):
+
+    mock_user = create_mock_user(mock_user_service, mock_db_session)
+
+    login = client.post(
+        LOGIN_ENDPOINT,
+        data={"username": "testuser", "password": "Testpassword@123"},
+    )
+    access_token = login.json()["data"]["access_token"]
+
+    user_pwd_change = client.patch(
+        CHANGE_PWD_ENDPOINT,
+        json={"old_password": "Testpassword@123", "new_password": "Ojobonandom@123"},
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
 
     assert user_pwd_change.status_code == 200
-    assert user_pwd_change.json()['message'] == 'Password Changed successfully'
-	
+    assert user_pwd_change.json()["message"] == "Password Changed successfully"
