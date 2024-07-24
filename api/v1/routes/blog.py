33009f34061a605da50f0907f331d5ae35adb0d8
Waptech <abdulfataiyakub07@gmail.com>
2024-07-24 13:10:50 +0100

    added prefix 'blogs/' & set get route to '/'

--- api/v1/routes/blog.py
+++ api/v1/routes/blog.py
@@ -6,9 +6,9 @@ from api.v1.models.blog import Blog
 from api.v1.schemas.blog import BlogResponse
 from api.db.database import get_db
 
-blog = APIRouter(tags=["Blog"])
+blog = APIRouter(prefix="/blogs", tags=["Blog"])
 
-@blog.get("/blogs", response_model=List[BlogResponse])
+@blog.get("/", response_model=List[BlogResponse])
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(Blog).filter(Blog.is_deleted == False).all()
     if not blogs:
