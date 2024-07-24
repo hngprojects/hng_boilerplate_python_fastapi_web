8afe408ab44e3cf25f5e942b47b4cdb89eb8b099
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-24 20:14:31 +0100

    added the Optional keyword to the tags

--- api/v1/schemas/blog.py
+++ api/v1/schemas/blog.py
@@ -21,16 +21,16 @@ class BlogResponse(BaseModel):
 
 
 class Blog(BaseModel):
- 
+
     author_id: str
     title: str
     content: str
     image_url: Optional[str]
     is_deleted: bool
     excerpt: Optional[str]
-    tags: [List[str]]
+    tags: Optional[List[str]]
     created_at: datetime
     updated_at: datetime
 
     class Config:
-        orm_mode = True
+        from_attributes = True
