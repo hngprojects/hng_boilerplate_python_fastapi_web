02d6f13fb234bda9fc9c4ad11fd0b3dbdace737d
segunUb18 <eeomo@yahoo.com>
2024-07-25 04:22:51 +0100

    :test_tube: fixing testcase

--- tests/v1/update_product_test.py
+++ tests/v1/update_product_test.py
@@ -20,9 +20,9 @@ def test_update_product_with_valid_token(mock_db, mocker):
     # Mock get_current_user
     mock_get_current_user = mocker.patch('api.utils.dependencies.get_current_user', return_value=User(id='user_id', is_super_admin=False))
     
-    # Mock the ProductModel
+    # Mock the Product model
     mock_product = MagicMock()
-    mock_product.id = 'e65c9f26-696a-42df-a4b9-4da21426734c'
+    mock_product.id = 'e65c9f26-696a-42df-a4b9-4da214267'
     mock_product.name = 'Old Product'
     mock_product.price = 20.0
     mock_product.description = 'Old Description'
@@ -38,28 +38,29 @@ def test_update_product_with_valid_token(mock_db, mocker):
     product_update = {
         "name": "Updated Product",
         "price": 25.0,
-        "description": "Updated Description"
+        "description": "Updated Description",
     }
     
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json=product_update, headers={"Authorization": "Bearer valid_token"})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da214267", json=product_update, headers={"Authorization": "Bearer valid_token"})
+    print(response.json()) 
     assert response.status_code == 200
     assert response.json()["name"] == "Updated Product"
     assert response.json()["price"] == 25.0
     assert response.json()["description"] == "Updated Description"
- 
+    assert response.json()["updated_at"] is not None  
 
 def test_update_product_without_token(mock_db, mocker):
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={"name": "Product"})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da214267", json={"name": "Product"})
     assert response.status_code == 401
 
 def test_update_product_with_invalid_token(mock_db, mocker):
     mock_get_current_user = mocker.patch('api.utils.dependencies.get_current_user', side_effect=HTTPException(status_code=401, detail="Invalid credentials"))
     
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={"name": "Product"}, headers={"Authorization": "Bearer invalid_token"})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={"name": "Product"}, headers={"Authorization": "Bearer invalid_token"})
     assert response.status_code == 401
 
 def test_update_product_with_missing_fields(mock_db, mocker):
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={})
     assert response.status_code == 400
     assert "name" in response.json()["detail"]
     assert "price" in response.json()["detail"]
@@ -67,7 +68,7 @@ def test_update_product_with_missing_fields(mock_db, mocker):
 def test_update_product_with_nonexistent_id(mock_db, mocker):
     mock_db().query().filter().first.return_value = None
     
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={"name": "Product", "price": 10.0, "description": "Description"}, headers={"Authorization": "Bearer valid_token"})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da21426734c", json={"name": "Product", "price": 10.0, "description": "Description"}, headers={"Authorization": "Bearer valid_token"})
     assert response.status_code == 404
 
 def test_update_product_with_special_characters(mock_db, mocker):
@@ -96,9 +97,10 @@ def test_update_product_with_special_characters(mock_db, mocker):
         "description": "Updated & Description!"
     }
     
-    response = client.put("/product/e65c9f26-696a-42df-a4b9-4da21426734c", json=product_update, headers={"Authorization": "Bearer valid_token"})
+    response = client.put("api/v1/product/e65c9f26-696a-42df-a4b9-4da21426734c", json=product_update, headers={"Authorization": "Bearer valid_token"})
+    print(response.json()) 
     assert response.status_code == 200
     assert response.json()["name"] == "Updated @Product! #2024"
     assert response.json()["price"] == 99.99
     assert response.json()["description"] == "Updated & Description!"
-    
+    assert response.json()["updated_at"] is not None
