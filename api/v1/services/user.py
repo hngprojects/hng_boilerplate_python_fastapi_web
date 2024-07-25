3e5776f07c88ef3286ac0c3a615b9b2f09690c66
kumdan job <106336086+trevorjob@users.noreply.github.com>
2024-07-25 21:08:48 +0100

    Update api/v1/services/user.py
    Co-authored-by: zxenonx <xenon.nemrod@gmail.com>
    Signed-off-by: kumdan job <106336086+trevorjob@users.noreply.github.com>

--- api/v1/services/user.py
+++ api/v1/services/user.py
@@ -165,8 +165,8 @@ class UserService(Service):
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
         data = {"user_id": user_id, "exp": expires, "type": "access"}
-        encoded_jwt = jwt.encode(data, settings.SECRET_KEY, settings.ALGORITHM)
-        return encoded_jwt
+        return  jwt.encode(data, settings.SECRET_KEY, settings.ALGORITHM)
+
 
     def create_refresh_token(self, user_id: str) -> str:
         """Function to create access token"""
