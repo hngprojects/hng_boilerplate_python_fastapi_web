5eecff8c2efc5d2807a80f9e77f0bf39641a651b
segunUb18 <eeomo@yahoo.com>
2024-07-25 17:11:08 +0100

    :fire: delete unrelated code and file

--- api/utils/dependencies.py
+++ api/utils/dependencies.py
@@ -15,24 +15,6 @@ from .config import SECRET_KEY, ALGORITHM
 # Initialize OAuth2PasswordBearer
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 
-# def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("username")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except JWTError:
-#         raise credentials_exception
-#     user = db.query(User).filter(User.username == token_data.username).first()
-#     if user is None:
-#         raise credentials_exception
-#     return user
 
 def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
