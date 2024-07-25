09978ee37b05acdcf496537313419659fbc2bba5
Ayobamidele Ewetuga <ayobamideleewetuga@gmail.com>
2024-07-25 22:18:39 +0100

    feat:fetch all billing plans

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -8,7 +8,7 @@ from api.v1.routes.notification import notification
 from api.v1.routes.testimonial import testimonial
 from api.v1.routes.facebook_login import fb_auth
 from api.v1.routes.blog import blog
-
+from api.v1.routes.billing_plan import bill_plan
 from api.v1.routes.waitlist import waitlist
 
 from api.v1.routes.notifications import notifications
@@ -27,3 +27,4 @@ api_version_one.include_router(testimonial)
 api_version_one.include_router(fb_auth)
 api_version_one.include_router(blog)
 api_version_one.include_router(waitlist)
+api_version_one.include_router(bill_plan)
