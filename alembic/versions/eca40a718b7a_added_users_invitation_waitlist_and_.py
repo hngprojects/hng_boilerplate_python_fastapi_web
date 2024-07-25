d53c34d34998a166d47dfb2232054645414890db
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-25 12:01:59 +0100

    propagated migration reference

--- alembic/versions/eca40a718b7a_added_users_invitation_waitlist_and_.py
+++ alembic/versions/eca40a718b7a_added_users_invitation_waitlist_and_.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'eca40a718b7a'
+revision: str = '05faae41758c'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
