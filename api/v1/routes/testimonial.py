a3a9baf15a003bcf1c7931a7e92cc164e48a3481
Yvonne Gichovi <133222720+yvonnegichovi@users.noreply.github.com>
2024-07-25 09:54:51 +0000

    Feat: fixing errors from a merge review

--- api/v1/routes/testimonial.py
+++ api/v1/routes/testimonial.py
@@ -5,7 +5,6 @@ from fastapi.responses import JSONResponse
 from api.utils.success_response import success_response
 from api.v1.models.testimonial import Testimonial
 from api.v1.models.user import User
-# from api.v1.schemas.user import DeactivateUserSchema, UserBase
 from api.db.database import get_db
 from api.v1.services.testimonial import testimonial_service
 from api.v1.services.user import user_service
@@ -50,15 +49,13 @@ async def delete_all_testimonials(db: Session = Depends(get_db)):
     Deletes all testimonials
     """
     try:
-        db.query(Testimonial).delete()
-        db.commit()
+        testimonial_service.delete_all(db)
         return JsonResponseDict(
             message="All testimonials deleted successfully",
             data={},
             status_code=status.HTTP_200_OK
         )
     except Exception as e:
-        db.rollback()
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=str(e)
