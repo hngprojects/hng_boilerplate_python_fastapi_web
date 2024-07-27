d2356a134180c4f08f3af2c26eae29e2528ee742
theijhay <olawaleisaacjohn@gmail.com>
2024-07-27 17:32:15 +0100

    Update jobs Listing

--- api/v1/routes/job.py
+++ api/v1/routes/job.py
@@ -8,18 +8,10 @@ from api.db.database import get_db
 from api.v1.models.user import User
 from api.v1.schemas.job import JobUpdate
 from api.v1.services.job_service import JobService
-from utils.json_response import JsonResponseDict
 from api.v1.routes.job import job
 
 job = APIRouter(prefix="/api/v1/jobs", tags=["jobs"])
 
-# Update a job post
-@job.patch(
-    "/{job_id}",
-    response_model=JsonResponseDict,
-    summary="Update job post",
-    description="This endpoint allows a user to update an existing job post by providing updated job details."
-)
 async def update_job_post(
     job_id: str,
     current_user: Annotated[User, Depends(get_current_user)],
