fd178767596f1c8d26dffd71df0e64dc216fc958
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-28 10:41:48 +0100

    Update plans.py
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

--- api/v1/schemas/plans.py
+++ api/v1/schemas/plans.py
@@ -1,17 +1,22 @@
+# schemas/plans.py
 from pydantic import BaseModel
 from typing import List, Optional
 import uuid
 
-class CreateSubscriptionPlan(BaseModel):
+class CreateBillingPlan(BaseModel):
     name: str
-    description: Optional[str] = None
-    price: int
-    duration: str
+    price: float
+    currency: str
     features: List[str]
-    
-    
-class SubscriptionPlanResponse(CreateSubscriptionPlan):
+    organization_id: str
+
+class BillingPlanResponse(BaseModel):
     id: uuid.UUID
-    
+    name: str
+    price: float
+    currency: str
+    features: List[str]
+    organization_id: str
+
     class Config:
-        orm_mode = True
+        orm_mode=True
