a201e5e6cc32da74a0cac51a508a5fad25e0d7bb
Mohammed Mubarak <modymu9@gmail.com>
2024-07-28 20:26:37 +0200

    fix: used success_response instead of JsonResponseTemplate

--- api/v1/routes/contact_us.py
+++ api/v1/routes/contact_us.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from api.db.database import get_db
 from typing import Annotated
 from api.core.responses import SUCCESS
-from api.utils.json_response import JsonResponseDict
+from api.utils.success_response import success_response
 from api.v1.services.contact_us import contact_service
 from api.v1.schemas.contact_us import CreateContactUs
 from api.v1.models import *
@@ -12,11 +12,11 @@ contact_us = APIRouter(tags=["Contact Us"])
 
 
 # CREATE
-@contact_us.post("/contact-us", response_model=JsonResponseDict.response)
+@contact_us.post("/contact-us", response_model=success_response)
 async def create_contact_us(data: CreateContactUs, db: Annotated[Session, Depends(get_db)]):
     """Add a new contact us message."""
     new_contact_us_message = contact_service.create(db, data)
-    response = JsonResponseDict(
+    response = success_response(
         message=SUCCESS,
         data={"id": new_contact_us_message.id},
         status_code=status.HTTP_201_CREATED,
