96abe8e2373ccf1071a951553e8925438902c219
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-28 18:11:29 +0100

    Create Plans.py
    merge it with the plans.py
    
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

---
+++ api/v1/schemas/Plans.py
@@ -0,0 +1,22 @@
+# schemas/plans.py
+from pydantic import BaseModel
+from typing import List, Optional
+import uuid
+
+class CreateBillingPlan(BaseModel):
+    name: str
+    price: float
+    currency: str
+    features: List[str]
+    organization_id: str
+
+class BillingPlanResponse(BaseModel):
+    id: uuid.UUID
+    name: str
+    price: float
+    currency: str
+    features: List[str]
+    organization_id: str
+
+    class Config:
+        from_attributes = True
