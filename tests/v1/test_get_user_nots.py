6287a0fb4332f15a75368652839164041fc8b975
kumdan job <redeks123456@gmail.com>
2024-07-28 17:57:59 +0100

    feat: added get user notification endpoint

--- tests/v1/test_get_user_nots.py
+++ tests/v1/test_get_user_nots.py
@@ -65,27 +65,26 @@ notification = Notification(
 )
 
 
-def test_mark_notification_as_read(client, db_session_mock):
+def test_get_all_notification_with_unauthenticated_user(client, db_session_mock):
+    # Create test notification
+
+    db_session_mock.query().filter().all.return_value = [notification]
+
+    response = client.patch(f"/api/v1/notifications/current-user")
+
+    assert response.status_code == 401
+    assert response.json()["success"] == False
+    assert response.json()["status_code"] == 401
+
+
+def test_get_notification_current_user(client, db_session_mock):
 
     db_session_mock.query().filter().all.return_value = [user, notification]
 
     headers = {"authorization": f"Bearer {access_token}"}
 
-    response = client.patch(f"/api/v1/notifications/{notification.id}", headers=headers)
+    response = client.patch(f"/api/v1/notifications/current-user", headers=headers)
 
     assert response.status_code == 200
     assert response.json()["success"] == True
     assert response.json()["status_code"] == 200
-    assert response.json()["message"] == "Notifcation marked as read"
-
-
-def test_mark_notification_as_read_unauthenticated_user(client, db_session_mock):
-    # Create test notification
-
-    db_session_mock.query().filter().all.return_value = [notification]
-
-    response = client.patch(f"/api/v1/notifications/{notification.id}")
-
-    assert response.status_code == 401
-    assert response.json()["success"] == False
-    assert response.json()["status_code"] == 401
