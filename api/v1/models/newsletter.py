27207664470b0283a12dfc6f045b12fd7d1d4e35
Frank Success <franksuccess95@gmail.com>
2024-07-28 19:36:52 +0100

    refactor: Modify Newsletter db model

--- api/v1/models/newsletter.py
+++ api/v1/models/newsletter.py
@@ -1,17 +1,29 @@
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, String, Text, Integer, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from api.v1.models.associations import user_newsletter_association
+# from api.v1.models.base import user_newsletter_association
 from api.v1.models.base_model import BaseTableModel
+from datetime import datetime, timezone
 
 
 class Newsletter(BaseTableModel):
-    """
-    Newsletter db model
-    """
     __tablename__ = 'newsletters'
 
-    email = Column(String(150), unique=True, nullable=False)
-    title = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text, nullable=True)
     content = Column(Text, nullable=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
-    subscribers = relationship("User", secondary=user_newsletter_association, back_populates="newsletters")
+    subscribers = relationship("NewsletterSubscriber", back_populates="newsletter")
+
+
+class NewsletterSubscriber(BaseTableModel):
+    __tablename__ = 'newsletter_subscribers'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), nullable=False)
+    newsletter_id = Column(Integer, ForeignKey('newsletters.id'))
+    subscribed_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    newsletter = relationship("Newsletter", back_populates="newsletter_subscribers")
