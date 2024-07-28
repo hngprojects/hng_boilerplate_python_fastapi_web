ee791cc603da3374c2a41a80b65e9367c2bb6d7b
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-28 10:43:57 +0100

    Update billing_plan.py
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

--- api/v1/routes/billing_plan.py
+++ api/v1/routes/billing_plan.py
@@ -1,36 +1,27 @@
-from fastapi import (
-    APIRouter,
-    Depends,
-    status,
-    )
+# routes/billing_plan.py
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 from api.v1.models.user import User
 from api.v1.services.billing_plan import billing_plan_service
 from api.db.database import get_db
 from api.utils.json_response import JsonResponseDict
 from api.v1.services.user import user_service
+from api.v1.schemas.plans import CreateBillingPlan, BillingPlanResponse
 
 bill_plan = APIRouter(prefix='/organizations', tags=['Billing-Plan'])
 
-@bill_plan.get('/billing-plans')
-async def retrieve_all_billing_plans(
-                     current_user: User = Depends(user_service.get_current_user),
-                                     db: Session = Depends(get_db)):
+@bill_plan.post('/billing-plans', response_model=BillingPlanResponse)
+async def create_billing_plan(
+    plan_data: CreateBillingPlan,
+    current_user: User = Depends(user_service.get_current_user),
+    db: Session = Depends(get_db)
+):
     """
-    Get All Billing Plan endpoint
+    Create a new Billing Plan endpoint
     """
-    billing_plans: list = [ {
-        "id": billing_plan.id,
-        "name": billing_plan.name,
-        "price": float(billing_plan.price),
-        "features": billing_plan.features,
-    } for billing_plan in billing_plan_service.fetch_all(db=db)]
-    
+    billing_plan = billing_plan_service.create_billing_plan(db=db, plan_data=plan_data)
     return JsonResponseDict(
-        status_code=status.HTTP_200_OK,
-        data={
-            "plans": billing_plans
-            },
-        message="plans fetched successfully"
-        
+        status_code=status.HTTP_201_CREATED,
+        data=billing_plan,
+        message="Billing plan created successfully"
     )
