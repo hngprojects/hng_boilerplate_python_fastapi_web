cca6e2b917b8a4de190fc39157f81da53035a587
NjengaC <victorcyrus01@gmail.com>
2024-07-29 09:18:51 +0300

    altered tests to match new changes

--- tests/v1/test_payment.py
+++ tests/v1/test_payment.py
@@ -4,6 +4,7 @@ from fastapi.testclient import TestClient
 from main import app
 from api.v1.services.payment import PaymentService
 from api.v1.schemas.payment import PaymentResponse
+from api.utils.db_validators import check_model_existence
 
 client = TestClient(app)
 
@@ -23,13 +24,13 @@ def test_get_payment(mocker):
     mocker.patch.object(PaymentService, 'get_payment_by_id', return_value=mock_payment)
 
     response = client.get(f"/api/v1/payments/{mock_payment['id']}")
-    assert response.status_code == 404
-# currently will return 404 not found until POST is implemented
-#    assert response.json() == PaymentResponse(**mock_payment).model_dump()
+    assert response.status_code == 200
+    assert response.json() == PaymentResponse(**mock_payment).model_dump()
 
 def test_get_payment_not_found(mocker):
-    mocker.patch.object(PaymentService, 'get_payment_by_id', return_value=None)
+    mocker.patch.object(PaymentService, 'get_payment_by_id', side_effect=HTTPException(status_code=404, detail='Payment does not exist'))
 
     response = client.get("/api/v1/payments/non_existent_id")
     assert response.status_code == 404
     assert response.json() == {'message': 'Payment does not exist', 'status_code': 404, 'success': False}
+
