0cc1e2203d0dd29ff81f08e11a9a4719fbfe46f1
theijhay <olawaleisaacjohn@gmail.com>
2024-07-29 02:06:06 +0100

    Get Org Details by ID

--- api/v1/routes/organisations.py
+++ api/v1/routes/organisations.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.v1.schemas.organisations import OrganizationResponse
 from api.v1.services.organisations import get_organization_by_id
+from api.v1.services.user import user_service, oauth2_scheme
 from api.v1.services.user import user_service, UserService
 from api.db.database import get_db
 
@@ -12,11 +13,6 @@ def get_current_user(token: str = Depends(UserService.oauth2_scheme), db: Sessio
     return user_service.get_current_user(token, db)
 
 @Organisation_id.get("/api/v1/organisations/{org_id}", response_model=OrganizationResponse)
-def read_organization(org_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
-    """
-    Fetch organization by ID.
-
-    - **org_id**: ID of the organization to fetch
-    - **current_user**: Automatically injected by FastAPI after verifying JWT token
-    """
+def read_organization(org_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    current_user = user_service.get_current_user(token, db)
     return get_organization_by_id(db, org_id)
