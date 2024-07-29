64cd6aec468570df3ad1cbb7687a310ce7d4e806
freemanboss <ibrahimgbagba800@gmail.com>
2024-07-29 21:45:07 +0000

    fixbug:fix-test-cases-and-refactoring-the-code#519

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -12,11 +12,13 @@ from api.v1.routes.billing_plan import bill_plan
 from api.v1.routes.google_login import google_auth
 from api.v1.routes.invitations import invites
 from api.v1.routes.profiles import profile
-from api.v1.routes.comment import comment
+from api.v1.routes.jobs import jobs
 from api.v1.routes.payment import payment
 from api.v1.routes.organization import organization
 from api.v1.routes.request_password import pwd_reset
 from api.v1.routes.activity_logs import activity_logs
+from api.v1.routes.contact_us import contact_us
+from api.v1.routes.comment import comment
 from api.v1.routes.user_organisations import user_organisations
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -34,8 +36,12 @@ api_version_one.include_router(bill_plan)
 api_version_one.include_router(google_auth)
 api_version_one.include_router(invites)
 api_version_one.include_router(profile)
+api_version_one.include_router(organization)
+api_version_one.include_router(pwd_reset)
 api_version_one.include_router(comment)
 api_version_one.include_router(payment)
-api_version_one.include_router(organization)
 api_version_one.include_router(activity_logs)
+api_version_one.include_router(contact_us)
+api_version_one.include_router(comment)
+api_version_one.include_router(jobs)
 api_version_one.include_router(user_organisations)
