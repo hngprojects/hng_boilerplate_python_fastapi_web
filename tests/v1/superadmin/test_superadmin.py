f4555152b43d7de849b6dc97164c1e455df99078
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2024-07-29 01:43:31 +0100

    chore: refactor superadmin

--- tests/v1/superadmin/test_superadmin.py
+++ tests/v1/superadmin/test_superadmin.py
@@ -10,13 +10,13 @@ from api.v1.models.user import User
 from api.v1.services.user import user_service, UserService
 from uuid_extensions import uuid7
 from api.db.database import get_db
-from fastapi import status, HTTPException
+from fastapi import status
 from datetime import datetime, timezone
 from sqlalchemy.orm import Session
 
 
 client = TestClient(app)
-USER_DELETE_ENDPOINT = "/api/v1/superadmin/users"
+USER_DELETE_ENDPOINT = "/api/v1/users"
 
 
 @pytest.fixture
@@ -29,7 +29,6 @@ def mock_db_session():
 
     with patch("api.v1.services.user.get_db", autospec=True) as mock_get_db:
         mock_db = MagicMock()
-        # mock_get_db.return_value.__enter__.return_value = mock_db
         app.dependency_overrides[get_db] = lambda: mock_db
         yield mock_db
     app.dependency_overrides = {}
@@ -94,6 +93,8 @@ def create_dummy_mock_user(mock_user_service: UserService, mock_db_session: Sess
     )
 
     mock_db_session.get.return_value = dummy_mock_user
+    mock_db_session.delete.return_value = None
+    mock_db_session.commit.return_value = None
 
 
 @pytest.mark.usefixtures("mock_db_session", "mock_user_service")
@@ -144,10 +145,15 @@ def test_successful_deletion(
 
     # Create a mock user
     create_dummy_mock_user(mock_user_service, mock_db_session)
+    mock_db_session.get.return_value = mock_db_session.get.return_value
+
     response = client.delete(
         f"{USER_DELETE_ENDPOINT}/{mock_id}",
     )
     assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # Simulate the user being deleted from the database
+    mock_db_session.get.return_value = None
 
     response = client.delete(
         f"{USER_DELETE_ENDPOINT}/{mock_id}",
@@ -164,6 +170,9 @@ def test_not_found_error(
     override_get_current_super_admin: None,
 ):
     """Test for invalid user ID"""
+
+    # Simulate the user not being found in the database
+    mock_db_session.get.return_value = None
 
     response = client.delete(
         f"{USER_DELETE_ENDPOINT}/{str(uuid7())}",
