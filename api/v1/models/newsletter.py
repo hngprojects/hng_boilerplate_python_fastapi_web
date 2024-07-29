d7e8ef68f55491cbd56eacaa84e74fce6a98d6ff
Frank Success <franksuccess95@gmail.com>
2024-07-29 09:44:04 +0100

    fix: Resolved conflicts

--- api/v1/models/newsletter.py
+++ api/v1/models/newsletter.py
@@ -7,12 +7,9 @@ from datetime import datetime, timezone
 class Newsletter(BaseTableModel):
     __tablename__ = 'newsletters'
 
-    id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
     content = Column(Text, nullable=True)
-    created_at = Column(DateTime, default=datetime.now(timezone.utc))
-    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     newsletter_subscribers = relationship("NewsletterSubscriber", back_populates="newsletter")
 
@@ -20,9 +17,7 @@ class Newsletter(BaseTableModel):
 class NewsletterSubscriber(BaseTableModel):
     __tablename__ = 'newsletter_subscribers'
 
-    id = Column(Integer, primary_key=True)
     email = Column(String(120), nullable=False)
     newsletter_id = Column(Integer, ForeignKey('newsletters.id'))
-    subscribed_at = Column(DateTime, default=datetime.now(timezone.utc))
 
     newsletter = relationship("Newsletter", back_populates="newsletter_subscribers")
