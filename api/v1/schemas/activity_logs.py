ce00d5e27742e6a16ae28c151311f91409f12862
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-07-29 07:27:17 +0100

    feat: create get activity log response schema

--- api/v1/schemas/activity_logs.py
+++ api/v1/schemas/activity_logs.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
+from datetime import datetime
+
 
 class ActivityLogCreate(BaseModel):
     user_id: int
     action: str
+
+
+class ActivityLogResponse(BaseModel):
+    action: str
+    timestamp: datetime
