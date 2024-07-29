22a7cdc841d2fb5ebad411c6304101f942b66202
theijhay <olawaleisaacjohn@gmail.com>
2024-07-29 02:24:07 +0100

    fixed pytest failures

--- api/v1/routes/organisations.py
+++ api/v1/routes/organisations.py
@@ -5,10 +5,10 @@ from api.v1.services.organisations import get_organization_by_id
 from api.v1.services.user import user_service, oauth2_scheme
 from api.db.database import get_db
 
-Organisation_id = APIRouter(prefix='/organisations', tags=['Organisations'])
+Organisation_id = APIRouter(prefix='/organisation', tags=['Organisation'])
 
 
-@Organisation_id.get("/api/v1/organisations/{org_id}", response_model=OrganizationResponse)
+@Organisation_id.get("/{org_id}", response_model=OrganizationResponse)
 def read_organization(org_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
     current_user = user_service.get_current_user(token, db)
     return get_organization_by_id(db, org_id)
