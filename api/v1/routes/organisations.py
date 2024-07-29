d65f2e6449e7aec91d39eb7e0b74d7007a540dab
theijhay <olawaleisaacjohn@gmail.com>
2024-07-29 02:08:06 +0100

    Get Org Details by ID

--- api/v1/routes/organisations.py
+++ api/v1/routes/organisations.py
@@ -3,14 +3,10 @@ from sqlalchemy.orm import Session
 from api.v1.schemas.organisations import OrganizationResponse
 from api.v1.services.organisations import get_organization_by_id
 from api.v1.services.user import user_service, oauth2_scheme
-from api.v1.services.user import user_service, UserService
 from api.db.database import get_db
 
 Organisation_id = APIRouter(prefix='/organisations', tags=['Organisations'])
 
-# Dependency to get the current user
-def get_current_user(token: str = Depends(UserService.oauth2_scheme), db: Session = Depends(get_db)):
-    return user_service.get_current_user(token, db)
 
 @Organisation_id.get("/api/v1/organisations/{org_id}", response_model=OrganizationResponse)
 def read_organization(org_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
