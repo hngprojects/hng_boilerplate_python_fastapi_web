cf6158be381777d99a2214f160efcb47c2f6d909
freemanboss <ibrahimgbagba800@gmail.com>
2024-07-29 14:40:55 +0000

    get-authenticated-user's-organisation-#519

--- api/v1/services/user_organisations.py
+++ api/v1/services/user_organisations.py
@@ -2,7 +2,7 @@ from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.db.database import get_db
 from api.v1.models.organization import Organization
-from api.v1.services.user import user_service
+from api.v1.services.user import user_service, oauth2_scheme
 from api.v1.models.user import User
 
 class UserOrganisationsService:
@@ -15,7 +15,7 @@ class UserOrganisationsService:
         return organizations
 
     def get_current_user(
-        self, access_token: str = Depends(user_service.oauth2_scheme), db: Session = Depends(get_db)
+        self, access_token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
     ) -> User:
         """Fetches the current user based on the provided access token."""
         return user_service.get_current_user(access_token=access_token, db=db)
