74b62a242182a43f116a69e3d12ce81158170de4
johnson-oragui <johnson-oragui@gmail.com>
2024-07-29 22:44:33 +0100

    fix: modified to verifying id_token

--- api/v1/routes/google_login.py
+++ api/v1/routes/google_login.py
@@ -1,14 +1,18 @@
-
+from typing import Annotated
+from sqlalchemy.orm import Session
 from fastapi import (APIRouter, Depends, status, HTTPException)
 
 from api.v1.services.google_oauth import GoogleOauthServices
-from api.v1.schemas.google_oauth import IdToken
+from api.v1.schemas.google_oauth import IdToken, Tokens
 from api.v1.services.google_oauth import GoogleOauthServices
+from api.db.database import get_db
+
 
 google_auth = APIRouter(prefix="/auth", tags=["Authentication"])
 
-@google_auth.post("/google-login")
-async def google_login(id_token: IdToken, google_service: GoogleOauthServices = Depends()):
+@google_auth.post("/google")
+async def google_login(id_token: IdToken, db: Annotated[Session, Depends(get_db)],
+                       google_service: GoogleOauthServices = Depends()):
     """
     Allows users to login using their google accounts.
 
@@ -18,9 +22,9 @@ async def google_login(id_token: IdToken, google_service: GoogleOauthServices = 
     Returns:
         Exception: if error occurs
     """
-    data = await google_service.verify_google_token(id_token.id_token)
+    access, refresh = google_service.verify_google_token(id_token.id_token, db)
     
-    if not data:
+    if not access or not refresh:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Login failed")
-    return data
-    
+    return Tokens(access_token=access,
+                  refresh_token=refresh)
