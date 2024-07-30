5270cb872b3eba70c51dfb5cfb9df7c9a7a00b0d
theijhay <olawaleisaacjohn@gmail.com>
2024-07-30 10:12:28 +0100

    fixed pytest errors

--- tests/v1/organization/test_organization.py
+++ tests/v1/organization/test_organization.py
@@ -71,16 +71,6 @@ def mock_get_current_user():
         yield
 
 def test_get_organization_success(db_session_mock, mock_get_current_user):
-    
-    # Mock the user service to return the current user
-    app.dependency_overrides[user_service.get_current_user] = lambda: mock_get_current_user
-    app.dependency_overrides[organization_service.get_organization] = lambda: mock_org
-    
-    # Mock organization creation
-    db_session_mock.add.return_value = None
-    db_session_mock.commit.return_value = None
-    db_session_mock.refresh.return_value = None
-    
     mock_organization = MagicMock(spec=Organization)
     mock_organization.id = 1
     mock_organization.company_name = "Mock Org 1"
@@ -94,7 +84,7 @@ def test_get_organization_success(db_session_mock, mock_get_current_user):
 
     db_session_mock.query().filter().first.return_value = mock_organization
 
-    with patch("api.v1.services.organization.organization_service.get_organization", return_value=db_session_mock):
+    with patch("api.v1.routes.organization.get_organization", return_value=db_session_mock):
         response = client.get("/api/v1/organizations/1", headers={"Authorization": "Bearer testtoken"})
         assert response.status_code == 200
         data = response.json()
