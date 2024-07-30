ec76d4a70a01b1f77caefed2f282806900d9b975
omar <153817122+OmarFatmi@users.noreply.github.com>
2024-07-30 10:19:17 +0100

    Update Billing_Plan.py
    get Plan By ID
    
    Signed-off-by: omar <153817122+OmarFatmi@users.noreply.github.com>

--- api/v1/services/Billing_Plan.py
+++ api/v1/services/Billing_Plan.py
@@ -10,5 +10,13 @@ class BillingPlanService:
         db.commit()
         db.refresh(billing_plan)
         return billing_plan
+        
+    def fetch_all(self, db: Session):
+        return db.query(BillingPlan).all()
+
+    def get_billing_plan_by_id(self, db: Session, plan_id: str) -> BillingPlan:
+        return db.query(BillingPlan).filter(BillingPlan.id == plan_id).first()
+
+
 
 billing_plan_service = BillingPlanService()
