d6e529617b985d8b65c46f1557ea4f490b03f8f6
johnson-oragui <johnson.oragui@gmail.com>
2024-07-30 22:55:27 +0100

    feat: Added filtering logic to filter users based on the pramas

--- api/v1/services/user.py
+++ api/v1/services/user.py
@@ -2,6 +2,7 @@ import random
 import string
 from typing import Any, Optional
 import datetime as dt
+from fastapi import status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, Request
@@ -36,21 +37,34 @@ class UserService(Service):
             per_page: max number of users in a page
             query_params: params to filter by
         """
-        per_page = per_page if per_page <= 10 else 10
-        query = (db.query(User)
-                 .order_by(desc(User.created_at))
-                 .limit(per_page)
-                 .offset((page - 1) * per_page))
-        total_users = db.query(User).count()
-
+        per_page = min(per_page, 10)
 
         # Enable filter by query parameter
-        if query_params:
-            query = db.query(User)
-            for column, value in query_params.items():
-                if hasattr(User, column) and value:
-                    query = query.filter(getattr(User, column).ilike(f"%{value}%"))
-        all_users: list = query.all()
+        filters = []
+        if all(query_params):
+            # Validate boolean query parameters
+            for param, value in query_params.items():
+                if value is not None and not isinstance(value, bool):
+                    raise HTTPException(
+                        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail=f"Invalid value for '{param}'. Must be a boolean."
+                    )
+                if value == None:
+                    continue
+                if hasattr(User, param):
+                    filters.append(getattr(User, param) == value)
+        query = db.query(User)
+        total_users = query.count()
+        if filters:
+            query = query.filter(*filters)
+            total_users = query.count()
+
+        all_users: list = (query
+                           .order_by(desc(User.created_at))
+                           .limit(per_page)
+                           .offset((page - 1) * per_page)
+                           .all())
+
         return self.all_users_response(all_users, total_users, page, per_page)
    
     def all_users_response(self, users: list, total_users: int,
@@ -62,7 +76,7 @@ class UserService(Service):
             total_users: total number of users
         """
         if not users or len(users) == 0:
-            return user.AllUsersResponse(message='No user has registered currently!',
+            return user.AllUsersResponse(message='No User(s) for this query',
                                          status='success',
                                          status_code=200,
                                          page=page,
