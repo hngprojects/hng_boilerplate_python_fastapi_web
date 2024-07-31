9133125048f12cda904f6b3cc9d500271dac5690
Modupe Akanni <modupe775@gmail.com>
2024-07-31 14:07:09 +0100

    fix: status format

--- api/utils/success_response.py
+++ api/utils/success_response.py
@@ -6,7 +6,7 @@ def success_response(status_code: int, message: str, data: Optional[dict] = None
     '''Returns a JSON response for success responses'''
 
     response_data = {
-        "status_code": status_code,
+        "status": status_code,
         "success": True,
         "message": message
     }
