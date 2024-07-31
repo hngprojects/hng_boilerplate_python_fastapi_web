d9363a02042d9590d700e332d641068daa967ffc
theijhay <olawaleisaacjohn@gmail.com>
2024-07-31 06:28:12 +0100

    made changes

--- tests/v1/organization/test_organization.py
+++ tests/v1/organization/test_organization.py
@@ -74,7 +74,7 @@ def test_get_organization_success(db_session_mock, mock_get_current_user):
 
     db_session_mock.query().filter().first.return_value = mock_organization
 
-    with patch("api.v1.routes.organization.fetch", return_value=mock_organization) as mock_fetch:
+    with patch("api.v1.routes.organization.get_db", return_value=mock_organization):
         response = client.get("/api/v1/organizations/1", headers={"Authorization": "Bearer testtoken"})
         assert response.status_code == 200
         data = response.json()
@@ -88,7 +88,7 @@ def test_get_organization_success(db_session_mock, mock_get_current_user):
 def test_get_organization_not_found(db_session_mock, mock_get_current_user):
     db_session_mock.query().filter().first.return_value = None
 
-    with patch("api.v1.routes.organization.fetch", return_value=db_session_mock):
+    with patch("api.v1.routes.organization.get_db", return_value=db_session_mock):
         response = client.get("/api/v1/organizations/999", headers={"Authorization": "Bearer testtoken"})
         assert response.status_code == 404
         data = response.json()
