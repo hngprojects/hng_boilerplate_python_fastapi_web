dee0b90417d55137085d5a01b5ec729d6308d010
freemanboss <ibrahimgbagba800@gmail.com>
2024-07-31 13:44:49 +0000

    fixing test

--- seed.py
+++ seed.py
@@ -1,30 +1,31 @@
-import sys, os
-import warnings
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from api.v1.models import *
-from api.v1.models.associations import Base
+from api.v1.models import User
 from api.v1.services.user import user_service
 from api.db.database import create_database, get_db
 
+# Create the database (uncomment if needed)
 # create_database()
+
 db = next(get_db())
 
-
+# Create an admin user
 admin_user = User(
     email="admin@example.com",
-    password=user_service.hash_password("supersecret"),
+    password=user_service.hash_password("supersecret"),  # Ensure this method exists and works correctly
     first_name="Admin",
     last_name="Habeeb",
-    user_name="Habeeb",
     is_active=True,
     is_super_admin=True,
     is_deleted=False,
     is_verified=True,
 )
-db.add(admin_user)
-db.commit()
 
-print("Seed data succesfully")
+# Add and commit the admin user to the database
+try:
+    db.add(admin_user)
+    db.commit()
+    print("Seed data successfully added")
+except Exception as e:
+    db.rollback()
+    print(f"An error occurred: {e}")
+finally:
+    db.close()
