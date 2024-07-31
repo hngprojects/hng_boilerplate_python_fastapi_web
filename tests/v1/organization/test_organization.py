ab8716582c3e71e4fc5c3a370f9866425f91fec9
theijhay <olawaleisaacjohn@gmail.com>
2024-07-31 16:26:26 +0100

    fixed pytest

--- tests/v1/organization/test_organization.py
+++ tests/v1/organization/test_organization.py
@@ -39,15 +39,17 @@ def mock_get_current_user():
 
 def test_get_organization_success(db_session_mock, mock_get_current_user):
     mock_organization = MagicMock(spec=Organization)
-    mock_organization.id = 1
+    mock_organization.id = "Str"
     mock_organization.company_name = "Mock Org 1"
-    mock_organization.company_email = "contact@mockorg1.com"
+    mock_organization.company_email = "olawaleisaacjohn@gmail.com"
     mock_organization.industry = "Technology"
     mock_organization.organization_type = "Private"
     mock_organization.country = "Country"
     mock_organization.state = "State"
     mock_organization.address = "123 Street"
     mock_organization.lga = "LGA"
+    mock_organization.created_at = datetime.now()
+    mock_organization.updated_at = datetime.now()
 
     db_session_mock.query().filter().first.return_value = mock_organization
 
@@ -56,11 +58,11 @@ def test_get_organization_success(db_session_mock, mock_get_current_user):
     data = response.json()
     assert data["status"] == "success"
     assert data["status_code"] == 200
-    assert data["data"]["id"] == 1
+    assert data["data"]["id"] == "Str"
     assert data["data"]["company_name"] == "Mock Org 1"
-    assert data["data"]["company_email"] == "contact@mockorg1.com"
+    assert data["data"]["company_email"] == "olawaleisaacjohn@gmail.com"
     assert data["data"]["industry"] == "Technology"
-    assert data["data"]["organization_type"] == "Private"
+    assert data["data"]["organization"] == "Private"
     assert data["data"]["state"] == "State"
     assert data["data"]["address"] == "123 Street"
     assert data["data"]["lga"] == "LGA"
