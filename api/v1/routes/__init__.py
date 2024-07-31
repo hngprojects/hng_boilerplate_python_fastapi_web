e862ddceaaf13fdc0ecf62539d9100ee70bc1ca1
Mohammed Mubarak <modymu9@gmail.com>
2024-07-31 23:59:01 +0200

    feat: added squeeze page create endpoint

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -25,6 +25,7 @@ from tests.run_all_test import test_router
 from api.v1.routes.topic import topic
 from api.v1.routes.notification_settings import notification_setting
 from api.v1.routes.api_tests import test_router
+from api.v1.routes.squeeze import squeeze
 
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -55,3 +56,4 @@ api_version_one.include_router(waitlist_router)
 api_version_one.include_router(newsletter)
 api_version_one.include_router(testimonial)
 api_version_one.include_router(test_router)
+api_version_one.include_router(squeeze)
