dc8d9e03a616fbdac1a9be712e8b94ad6b19cab3
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-07-31 19:04:50 +0100

    added pydantic model

--- api/v1/routes/reply.py
+++ api/v1/routes/reply.py
@@ -1,10 +1,12 @@
 from api.v1.models.reply import Reply
 from fastapi import APIRouter
+from api.db.database import get_db
+from api.v1.schemas.reply import ReplyCreate
 
-reply_router = APIRouter('/comments', tags=['comments_reply'])
+reply_router = APIRouter(prefix='/comments', tags=['comments_reply'])
 
-@reply_router.post('/<string:comment_id>/reply', status_code=200)
-async def reply_to_a_comment():
+@reply_router.post('/{comment_id}/reply', status_code=200)
+async def reply_to_a_comment(comment_id: str, reply: ReplyCreate):
     """
     """
     pass
