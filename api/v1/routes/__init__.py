5c2753e76f6ed20ff2bd8f513d53064f22c90967
Chime <chimeobioha@gmail.com>
2024-08-01 16:11:54 +0100

    Add feature/dashboard-products, with tests

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -28,6 +28,7 @@ from api.v1.routes.regions import regions
 from api.v1.routes.api_tests import test_router
 from api.v1.routes.email_routes import email_sender
 from api.v1.routes.squeeze import squeeze
+from api.v1.routes.dashboard import dashboard
 
 
 api_version_one = APIRouter(prefix="/api/v1")
@@ -62,3 +63,4 @@ api_version_one.include_router(email_sender)
 api_version_one.include_router(regions)
 api_version_one.include_router(test_router)
 api_version_one.include_router(squeeze)
+api_version_one.include_router(dashboard)
