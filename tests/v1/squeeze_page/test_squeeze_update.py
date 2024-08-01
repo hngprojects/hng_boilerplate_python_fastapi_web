957ccc773abb9840a904395a794be3cf6bcd27e6
Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>
2024-08-01 14:14:17 +0100

    Update test_squeeze_update.py
    Signed-off-by: Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>

--- tests/v1/squeeze_page/test_squeeze_update.py
+++ tests/v1/squeeze_page/test_squeeze_update.py
@@ -5,7 +5,7 @@ from api.db.database import get_db
 from unittest.mock import MagicMock, patch
 from api.v1.models.user import User
 from api.v1.services.user import user_service
-from api.v1.schema.squeeze import CreateSqueeze, UpdateSqueeze 
+from api.v1.schemas.squeeze import CreateSqueeze, UpdateSqueeze 
 from uuid_extensions import uuid7
 from fastapi import status
 
