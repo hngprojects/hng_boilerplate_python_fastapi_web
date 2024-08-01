47681c947c2792c57579ffae5d7a76137505a04b
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-01 16:09:54 +0100

    fix: Uncomment code and imports from routes __init__.py file

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -15,11 +15,11 @@ from api.v1.routes.profiles import profile
 from api.v1.routes.jobs import jobs
 from api.v1.routes.payment import payment
 from api.v1.routes.organization import organization
-# from api.v1.routes.request_password import pwd_reset
+from api.v1.routes.request_password import pwd_reset
 from api.v1.routes.activity_logs import activity_logs
 from api.v1.routes.contact_us import contact_us
 from api.v1.routes.comment import comment
-# from api.v1.routes.sms_twilio import sms
+from api.v1.routes.sms_twilio import sms
 from api.v1.routes.faq import faq
 import api.v1.routes.payment_flutterwave
 
@@ -28,7 +28,7 @@ from api.v1.routes.topic import topic
 from api.v1.routes.notification_settings import notification_setting
 from api.v1.routes.regions import regions
 from api.v1.routes.api_tests import test_router
-# from api.v1.routes.email_routes import email_sender
+from api.v1.routes.email_routes import email_sender
 from api.v1.routes.squeeze import squeeze
 from api.v1.routes.email_template import email_template
 
@@ -38,7 +38,7 @@ api_version_one = APIRouter(prefix="/api/v1")
 api_version_one.include_router(auth)
 api_version_one.include_router(google_auth)
 api_version_one.include_router(fb_auth)
-# api_version_one.include_router(pwd_reset)
+api_version_one.include_router(pwd_reset)
 api_version_one.include_router(user)
 api_version_one.include_router(profile)
 api_version_one.include_router(organization)
@@ -52,7 +52,7 @@ api_version_one.include_router(invites)
 api_version_one.include_router(activity_logs)
 api_version_one.include_router(blog)
 api_version_one.include_router(comment)
-# api_version_one.include_router(sms)
+api_version_one.include_router(sms)
 api_version_one.include_router(jobs)
 api_version_one.include_router(test_router)
 api_version_one.include_router(faq)
@@ -62,7 +62,7 @@ api_version_one.include_router(waitlist_router)
 api_version_one.include_router(newsletter)
 api_version_one.include_router(testimonial)
 api_version_one.include_router(test_rout)
-# api_version_one.include_router(email_sender)
+api_version_one.include_router(email_sender)
 api_version_one.include_router(regions)
 api_version_one.include_router(test_router)
 api_version_one.include_router(squeeze)
