c4c12aef06724d3a9d73fcccb9b48b124d55380d
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-08-01 14:59:06 +0100

    Added foreignKey relationship

--- api/v1/models/reply.py
+++ api/v1/models/reply.py
@@ -1,5 +1,6 @@
 from sqlalchemy import String, Column, Text, ForeignKey
 from api.v1.models.base_model import BaseTableModel
+from sqlalchemy.orm import relationship
 
 
 class Reply(BaseTableModel):
@@ -9,6 +10,7 @@ class Reply(BaseTableModel):
     __tablename__ = "replies"
     
     user_id = Column(String, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
-    comment_id = Column(String)
+    comment_id = Column(String, ForeignKey('comments.id'), nullable=False)
     content = Column(Text, nullable=False)
+    comment = relationship("Comment", back_populates="replies")
     
