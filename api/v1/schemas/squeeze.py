7a75c4ab97fc6365b48cc4583d606b2c6a5d8ab2
Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>
2024-08-01 13:04:32 +0100

    Update squeeze.py
    Signed-off-by: Olusegun Emmanuel <91101596+CodewithSegNet@users.noreply.github.com>

--- api/v1/schemas/squeeze.py
+++ api/v1/schemas/squeeze.py
@@ -17,3 +17,16 @@ class CreateSqueeze(BaseModel):
     type: str = "product"
     status: SqueezeStatusEnum = SqueezeStatusEnum.offline
     full_name: str = None
+
+
+
+class UpdateSqueeze(BaseModel):
+    title: Optional[str] = None
+    email: Optional[EmailStr] = None
+    url_slug: Optional[str] = None
+    headline: Optional[str] = None
+    sub_headline: Optional[str] = None
+    body: Optional[str] = None
+    type: Optional[str] = None
+    status: Optional[SqueezeStatusEnum] = None
+    full_name: Optional[str] = None
