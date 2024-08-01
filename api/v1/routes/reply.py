c3299b08b740b7393b20e223a35afe93e85d5eb6
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-08-01 20:50:16 +0100

    successfully implementation of reply functionality

--- api/v1/routes/reply.py
+++ api/v1/routes/reply.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, status
 from api.db.database import get_db
 from api.v1.schemas.reply import ReplyCreate
 from sqlalchemy.orm import Session
-from api.v1.services.reply import ReplyService
+from api.v1.services.reply import reply_service
 from api.v1.models.user import User
 from api.v1.services.user import user_service
 from api.utils.success_response import success_response
@@ -13,13 +13,12 @@ from api.utils.success_response import success_response
 reply_router = APIRouter(prefix='/comments', tags=['comments_reply'])
 
 @reply_router.post('/{comment_id}/reply', response_model=success_response, status_code=status.HTTP_201_CREATED)
-async def reply_to_a_comment(comment_id: str, reply: ReplyCreate, db: Session = Depends(get_db), current_user: User = Depends(user_service.get_current_user)):
+async def reply_to_a_comment(comment_id: str, schema: ReplyCreate, db: Session = Depends(get_db), current_user: User = Depends(user_service.get_current_user)):
     """
     """
-    # calling current user just to ensure user is validated
-    current_user
+    user_id = current_user.id
     
-    new_reply = ReplyService.create(db=db, schema=reply, comment_id=comment_id)
+    new_reply = reply_service.create(schema=schema, comment_id=comment_id, user_id=user_id, db=db)
     
     # Possible Python Bug Alert!
     # for some reason, the below line is needed to enable data to be included in response
