eb9671507ce1058ecd86ce06d1004ea7f23ccac1
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-08-01 20:51:09 +0100

    implemented abstract method

--- api/v1/services/reply.py
+++ api/v1/services/reply.py
@@ -8,12 +8,28 @@ class ReplyService(Service):
     """
     """ 
     
-    def create(self, db: Session, schema: ReplyCreate):
+    # @staticmethod
+    def create(self, db: Session, schema: ReplyCreate, comment_id: str, user_id: str):
         """
         """
-        reply = Reply(**schema.model_dump())
+        reply = Reply(**schema.model_dump(), user_id=user_id, comment_id=comment_id)
         db.add(reply)
         db.commit()
         db.refresh(reply)
         
         return reply
+    
+    def fetch(self, db: Session):
+        super().fetch()
+
+    def fetch_all(self):
+        super().fetch_all()
+
+    def update(self):
+        super().update()
+
+    def delete(self):
+        return super().delete()
+    
+    
+reply_service = ReplyService()
