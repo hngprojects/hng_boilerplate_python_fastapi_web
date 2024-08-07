74e281ade8435d62232d0e7afe5c7f6cfbb576d9
Johnson Oragui <johnson.oragui@gmail.com>
2024-08-07 18:56:14 +0100

    fix: updated migrations to remove relationship with product variants
    Signed-off-by: Johnson Oragui  <johnson.oragui@gmail.com>

--- alembic/versions/c3b40dad6233_added_product_comments_table_relate_.py
+++ alembic/versions/c3b40dad6233_added_product_comments_table_relate_.py
@@ -29,7 +29,6 @@ def upgrade() -> None:
     sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
     sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
     sa.ForeignKeyConstraint(['product_id'], ['products.id'], ondelete='CASCADE'),
-    sa.ForeignKeyConstraint(['product_variant_id'], ['product_variants.id'], ondelete='CASCADE'),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
     sa.PrimaryKeyConstraint('id')
     )
