342c61dfd47a7e80af95e1eb3d6809003232628c
Arnthorny <chidubem.anthony26@gmail.com>
2024-08-07 16:54:09 +0100

    feat: Endpoint that allows a superadmin delete an invitation from db

--- api/v1/routes/invitations.py
+++ api/v1/routes/invitations.py
@@ -5,6 +5,7 @@ from api.v1.schemas import invitations
 from api.db.database import get_db as get_session
 from api.v1.services import invite
 from api.v1.models.user import User
+from api.utils.success_response import success_response
 from api.v1.services.user import user_service
 import logging
 
@@ -43,3 +44,26 @@ async def add_user_to_organization(
     logging.info(f"Processing invitation ID: {invite_id}")
 
     return invite.InviteService.add_user_to_organization(invite_id, session)
+
+@invites.delete("/{invite_id}", status_code=200, response_model=success_response)
+def delete_invite(
+    invite_id: str,
+    db: Session = Depends(get_session), 
+    admin: User = Depends(user_service.get_current_super_admin)
+):
+    """ Delete invite from database """
+    if invite_id.strip() == "":
+        logging.warning(f"Invitation ID not found in path parameter.")
+        raise HTTPException(status_code=404, detail="Invite id is absent")
+    
+    invite_is_deleted = invite.InviteService.delete(db, invite_id)
+
+    if not invite_is_deleted:
+        raise HTTPException(status_code=404, detail="Invalid invitation id")
+
+    logging.info(f"Deleted invite. ID: {invite_id}")
+
+    return success_response(
+        status_code=200,
+        message='Invite deleted successfully',
+    )
