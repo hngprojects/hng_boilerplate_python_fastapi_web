93e246cc9d2e62964ce4c64d0324e4176d4c6ae9
segunUb18 <eeomo@yahoo.com>
2024-08-07 17:50:35 +0100

    :ambulance: made changes required

--- api/v1/routes/permissions/roles.py
+++ api/v1/routes/permissions/roles.py
@@ -40,13 +40,9 @@ def delete_role(
     db: Session = Depends(get_db),
     current_user: User = Depends(user_service.get_current_user)
 ):
-    try:
-        role_service.delete_role(db, role_id)
-        return success_response(status_code=200, message="Role successfully deleted.", data={"id": role_id})
-    except HTTPException as e:
-        return JSONResponse(status_code=e.status_code, content={"status": False, "status_code": e.status_code, "message": e.detail})
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"status": False, "status_code": 500, "message": f"An unexpected error occurred: {str(e)}"})
+    role_service.delete_role(db, role_id)
+    return success_response(status_code=200, message="Role successfully deleted.", data={"id": role_id})
+       
     
     
     
