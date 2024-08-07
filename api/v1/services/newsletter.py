d97544870b5a3214b08b836d5ad6ba0566bd1344
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-08-07 13:18:31 +0100

    feat: add a fetch and delete methods for newsletter service

--- api/v1/services/newsletter.py
+++ api/v1/services/newsletter.py
@@ -2,18 +2,19 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from api.v1.schemas.newsletter import EmailSchema
 from api.core.base.services import Service
-from api.v1.models.newsletter import NewsletterSubscriber
+from api.v1.models.newsletter import NewsletterSubscriber, Newsletter
 from typing import Optional, Any
+from api.utils.db_validators import check_model_existence
+
 
 class NewsletterService(Service):
     """Newsletter service functionality"""
 
     @staticmethod
     def create(db: Session, request: EmailSchema) -> NewsletterSubscriber:
-        '''add a new subscriber'''
+        """add a new subscriber"""
 
-        new_subscriber = NewsletterSubscriber(
-            email=request.email)
+        new_subscriber = NewsletterSubscriber(email=request.email)
         db.add(new_subscriber)
         db.commit()
         db.refresh(new_subscriber)
@@ -21,22 +22,33 @@ class NewsletterService(Service):
         return new_subscriber
 
     @staticmethod
-    def check_existing_subscriber(db: Session, request: EmailSchema) -> NewsletterSubscriber:
+    def check_existing_subscriber(
+        db: Session, request: EmailSchema
+    ) -> NewsletterSubscriber:
         """
         Check if user with email already exist
         """
 
-        newsletter = db.query(NewsletterSubscriber).filter(NewsletterSubscriber.email==request.email).first()
+        newsletter = (
+            db.query(NewsletterSubscriber)
+            .filter(NewsletterSubscriber.email == request.email)
+            .first()
+        )
         if newsletter:
             raise HTTPException(
                 status_code=400, detail="User already subscribed to newsletter"
             )
 
         return newsletter
-    
+
+    @staticmethod
+    def fetch(self, db: Session, id: str):
+        """Fetches a single newsletter by id"""
+        return check_model_existence(db=db, model=Newsletter, id=id)
+
     @staticmethod
     def fetch_all(db: Session, **query_params: Optional[Any]):
-        '''Fetch all newsletter subscriptions with option to search using query parameters'''
+        """Fetch all newsletter subscriptions with option to search using query parameters"""
 
         query = db.query(NewsletterSubscriber)
 
@@ -44,6 +56,17 @@ class NewsletterService(Service):
         if query_params:
             for column, value in query_params.items():
                 if hasattr(NewsletterSubscriber, column) and value:
-                    query = query.filter(getattr(NewsletterSubscriber, column).ilike(f'%{value}%'))
+                    query = query.filter(
+                        getattr(NewsletterSubscriber, column).ilike(f"%{value}%")
+                    )
 
         return query.all()
+
+    @staticmethod
+    def delete(self, db: Session, id: str):
+        """Deletes a single newsletter by id"""
+
+        newsletter = self.fetch(db=db, id=id)
+
+        db.delete(newsletter)
+        db.commit()
