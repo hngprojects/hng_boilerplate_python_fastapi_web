0b4a42645d710eaf3d92696949e944a99f39cf46
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2024-08-08 18:06:28 +0100

    Propagated revision version

--- alembic/versions/1df47466d698_added_replies_table.py
+++ alembic/versions/1df47466d698_added_replies_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '1df47466d698'
-down_revision: Union[str, None] = '854472eb449d'
+down_revision: Union[str, None] = '69eb297622a4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
