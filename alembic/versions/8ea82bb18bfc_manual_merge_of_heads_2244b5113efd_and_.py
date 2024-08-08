510ce58a67c02b4b898a4ca9011b64fc15e9b93c
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-08-08 04:34:01 +0100

    fix:alembic heads

--- alembic/versions/8ea82bb18bfc_manual_merge_of_heads_2244b5113efd_and_.py
+++ alembic/versions/8ea82bb18bfc_manual_merge_of_heads_2244b5113efd_and_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '8ea82bb18bfc'
-down_revision: Union[str, None] = 'c1012aa6f30c'
+down_revision: Union[str, None] = ('2244b5113efd', 'c1012aa6f30c')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
