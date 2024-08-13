dfed589ba30aaa16d5669fd39b9d5e7fc8e1e525
johnson-oragui <johnson-oragui@gmail.com>
2024-08-13 20:43:57 +0100

    feat: added reset_password_tokens, user.reset_password_token, and reset_password_token.user_id

--- api/v1/models/user.py
+++ api/v1/models/user.py
@@ -89,6 +89,10 @@ class User(BaseTableModel):
     subscriptions = relationship(
         "UserSubscription", back_populates="user", cascade="all, delete-orphan"
     )
+
+    reset_password_token = relationship("ResetPasswordToken",
+                                        back_populates="user",
+                                        cascade="all, delete-orphan")
     
     def to_dict(self):
         obj_dict = super().to_dict()
