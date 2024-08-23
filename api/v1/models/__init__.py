4f284fa2b788ed822d58095252e875fa72c27d2d
Joshua Oloton <olotonjoshua@gmail.com>
2024-08-23 04:06:20 +0100

    feat: post user faq-inquiries form

--- api/v1/models/__init__.py
+++ api/v1/models/__init__.py
@@ -28,3 +28,4 @@ from api.v1.models.data_privacy import DataPrivacySetting
 from api.v1.models.privacy import PrivacyPolicy
 from api.v1.models.terms import TermsAndConditions
 from api.v1.models.reset_password_token import ResetPasswordToken
+from api.v1.models.faq_inquiries import FAQInquiries
