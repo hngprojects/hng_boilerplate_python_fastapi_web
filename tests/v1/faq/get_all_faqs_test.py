c1fe3b9d64276734ab7f8752428ab27f433bed8b
Oluwanifemi <latunde.nifemi2016@gmail.com>
2024-08-24 12:38:02 +0100

    feat: implement returning faqs by category

--- tests/v1/faq/get_all_faqs_test.py
+++ tests/v1/faq/get_all_faqs_test.py
@@ -1,11 +1,10 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 from api.db.database import get_db
-from api.v1.models.faq import FAQ
 from api.v1.services.faq import faq_service
 from main import app
 
@@ -15,6 +14,7 @@ def mock_db_session():
     db_session = MagicMock(spec=Session)
     return db_session
 
+
 @pytest.fixture
 def client(mock_db_session):
     app.dependency_overrides[get_db] = lambda: mock_db_session
@@ -23,19 +23,24 @@ def client(mock_db_session):
     app.dependency_overrides = {}
 
 
-def test_get_all_faqs(mock_db_session, client):
-    """Test to verify the pagination response for FAQs."""
-    # Mock data
-    mock_faq_data = [
-        FAQ(id=1, question="Question 1", answer="Answer 1"),
-        FAQ(id=2, question="Question 2", answer="Answer 2"),
-        FAQ(id=3, question="Question 3", answer="Answer 3"),
-    ]
+def test_get_all_faqs_grouped_by_category(mock_db_session, client):
+    """Test to verify the response for FAQs grouped by category."""
 
-    app.dependency_overrides[faq_service.fetch_all] = mock_faq_data
+    mock_faq_data_grouped = {
+        "General": [
+            {"question": "What is FastAPI?",
+                "answer": "FastAPI is a modern web framework for Python."},
+            {"question": "What is SQLAlchemy?",
+                "answer": "SQLAlchemy is a SQL toolkit and ORM for Python."}
+        ],
+        "Billing": [
+            {"question": "How do I update my billing information?",
+                "answer": "You can update your billing information in the account settings."}
+        ]
+    }
 
-    # Perform the GET request
-    response = client.get('/api/v1/faqs')
+    with patch.object(faq_service, 'fetch_all_grouped_by_category', return_value=mock_faq_data_grouped):
 
-    # Verify the response
+        response = client.get('/api/v1/faqs')
+
     assert response.status_code == 200
