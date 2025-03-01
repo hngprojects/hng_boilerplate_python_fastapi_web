aa7af95fedb290e1ac3189afca94cb672b25b623
tha_orakkle <adegbiranayinoluwa.paul@yahoo.com>
2025-03-01 17:34:35 +0100

    update: make /register and /login endpoint to create a user session on request

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -1,4 +1,5 @@
 import logging
+import datetime as dt
 from datetime import timedelta
 from fastapi.responses import JSONResponse
 from jose import ExpiredSignatureError, JWTError
@@ -21,6 +22,8 @@ from typing import Annotated
 from api.core.dependencies.email_sender import send_email
 from api.utils.success_response import auth_response, success_response
 from api.utils.send_mail import send_magic_link
+from api.utils.settings import settings
+from api.utils.session_helpers import get_session_schema_data
 from api.v1.models import User
 from api.v1.schemas.user import Token, UserEmailSender
 from api.v1.schemas.user import (
@@ -31,7 +34,7 @@ from api.v1.schemas.user import (
     UserData2,
 )
 from api.v1.schemas.token import TokenRequest
-
+from api.v1.schemas.session import SessionCreate
 from api.v1.schemas.user import (MagicLinkRequest,
                                  ChangePasswordSchema,
                                  AuthMeResponse)
@@ -50,6 +53,7 @@ from api.v1.schemas.totp_device import (
 )
 from api.v1.services.totp import totp_service
 from api.utils.settings import settings
+from api.v1.services.session import SessionService
 
 auth = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -94,7 +98,15 @@ def register(
     access_token = user_service.create_access_token(user_id=user.id)
     refresh_token = user_service.create_refresh_token(user_id=user.id)
     cta_link = f"{settings.ANCHOR_PYTHON_BASE_URL}/about-us"
-
+    expires = dt.datetime.now(dt.timezone.utc) + (dt.timedelta(
+        days=settings.JWT_REFRESH_EXPIRY) - dt.timedelta(seconds=1)
+    )
+    session_schema: SessionCreate = get_session_schema_data(
+        request,
+        refresh_token=refresh_token,
+        expires_at=str(expires))
+    session_service = SessionService(db)
+    session_service.create(db=db, schema=session_schema, user_id=user.id)
 
     # Send email in the background
     background_tasks.add_task(
@@ -245,6 +257,15 @@ def login(request: Request, login_request: LoginRequest, background_tasks: Backg
     # Generate access and refresh tokens
     access_token = user_service.create_access_token(user_id=user.id)
     refresh_token = user_service.create_refresh_token(user_id=user.id)
+    expires = dt.datetime.now(dt.timezone.utc) + (dt.timedelta(
+        days=settings.JWT_REFRESH_EXPIRY) - dt.timedelta(seconds=1)
+    )
+    session_schema: SessionCreate = get_session_schema_data(
+        request,
+        refresh_token=refresh_token,
+        expires_at=str(expires))
+    session_service = SessionService(db)
+    session_service.create(db=db, schema=session_schema, user_id=user.id)
 
     # Background task for email notification
     logger.info(f"Queueing login notification for {user.email} in the background...")
