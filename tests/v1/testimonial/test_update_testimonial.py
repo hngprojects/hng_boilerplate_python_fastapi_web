ba0dbf4840011cc2fd8e452c6d5fc70662b8bb79
El <elijahdenis540@gmail.com>
2025-03-02 07:22:34 +0100

    fix: added a function to create testimonial and doctrings

--- tests/v1/testimonial/test_update_testimonial.py
+++ tests/v1/testimonial/test_update_testimonial.py
@@ -3,6 +3,7 @@ import pytest
 from main import app
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock, patch
+from api.v1.models import Testimonial  # noqa: F403
 
 client = TestClient(app)
 
@@ -23,6 +24,7 @@ data = [
 
 @pytest.fixture(scope='module')
 def mock_send_mail():
+    """ For mocking the email send functionality """
     with patch("api.core.dependencies.email_sender.send_email") as mock_email_sending:
         with patch("fastapi.BackgroundTasks.add_task") as add_task_mock:
             add_task_mock.side_effect = lambda func, *args, **kwargs: func(*args, **kwargs)
@@ -31,6 +33,7 @@ def mock_send_mail():
 
 @pytest.fixture(scope="function")
 def client_with_mocks(mock_send_mail):
+    """For mocking a database connection and returning a test client"""
     with patch('api.db.database.get_db') as mock_get_db:
         mock_db = MagicMock()
         mock_get_db.return_value = mock_db
@@ -44,6 +47,7 @@ def client_with_mocks(mock_send_mail):
 
 @pytest.fixture(autouse=True)
 def setup_access_token(client_with_mocks):
+    """For setting up an access token for authentication"""
     client, mock_db = client_with_mocks
     mock_db.query.return_value.filter.return_value.first.return_value = None
 
@@ -62,24 +66,52 @@ def setup_access_token(client_with_mocks):
     return user_response.json()["data"]["access_token"]
 
 
-def test_update_testimonial_success(client_with_mocks, setup_access_token):
+def test_create_testimonial(client_with_mocks, setup_access_token):
+    """Test to create testimonial"""
     client, mock_db = client_with_mocks
 
+    payload = {
+        "content": "I love python",
+        "ratings": 5.02,
+    }
+
+    response = client.post(
+        "api/v1/testimonials/",
+        json=payload,
+        headers={"Authorization": setup_access_token},
+    )
+
+    assert response.status_code == 201
+
+    testimonial_id = response.json()["data"]["id"]
+    testimonial = MagicMock()
+    testimonial.content = payload["content"]
+    testimonial.ratings = payload["ratings"]
+
+    mock_db.query(Testimonial).get.return_value = testimonial
+    retrieved_testimonial = mock_db.query(Testimonial).get(testimonial_id)
+
+    assert retrieved_testimonial.content == payload["content"]
+    assert retrieved_testimonial.ratings == payload["ratings"]
+
+    return testimonial_id
+
+
+def test_update_testimonial_success(client_with_mocks, setup_access_token):
+    """Test to update testimonial successfully"""
+    client, mock_db = client_with_mocks
+
+    testimonial_id = test_create_testimonial(client_with_mocks, setup_access_token)
+
     mock_testimonial = MagicMock()
-    mock_testimonial.id = uuid.UUID(data[0]["id"])
+    mock_testimonial.id = uuid.UUID(testimonial_id)
     mock_testimonial.content = data[0]["content"]
     mock_testimonial.client_name = data[0]["client_name"]
     mock_testimonial.client_designation = data[0]["client_designation"]
     mock_testimonial.comments = data[0]["comments"]
     mock_testimonial.ratings = data[0]["ratings"]
 
-    mock_query = mock_db.query.return_value
-    mock_filter = mock_query.filter.return_value
-    mock_first = mock_filter.first
-    mock_first.return_value = mock_testimonial
-
-    print("Mock Testimonial: ", mock_first.return_value)
-    print("Access Token:", setup_access_token)
+    mock_db.query.return_value.filter.return_value.first.return_value = mock_testimonial
 
     update_data = {"content": "I love python (updated)"}
 
@@ -94,6 +126,7 @@ def test_update_testimonial_success(client_with_mocks, setup_access_token):
 
 
 def test_update_testimonial_not_found(client_with_mocks, setup_access_token):
+    """Test to update testimonial that does not exist"""
     client, mock_db = client_with_mocks
 
     mock_db.query.return_value.filter.return_value.first.return_value = None
@@ -109,6 +142,7 @@ def test_update_testimonial_not_found(client_with_mocks, setup_access_token):
 
 
 def test_update_testimonial_unauthorized(client_with_mocks):
+    """Test to update testimonial without authorization"""
     client, _ = client_with_mocks
 
     response = client.put(
