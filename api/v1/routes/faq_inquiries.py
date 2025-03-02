2addc3e79d7dc01480994cab56ef219f650d1c1c
Bobbysam <samuelt.oshin@gmail.com>
2025-03-02 13:43:01 +0100

    refractor(feature.py): updated the delete and create endpoint to use dependency

--- api/v1/routes/faq_inquiries.py
+++ api/v1/routes/faq_inquiries.py
@@ -14,43 +14,44 @@ from fastapi import Request
 faq_inquiries = APIRouter(prefix="/faq-inquiries", tags=["FAQ-Inquiries"])
 
 # CREATE
-@faq_inquiries.post(
-    "",
-    response_model=success_response,
-    status_code=status.HTTP_201_CREATED,
-    responses={
-        201: {"description": "FAQ Inquiry created successfully"},
-        422: {"description": "Validation Error"},
-    },
-)
-async def create_faq_inquiry(
-    data: CreateFAQInquiry,
-    request: Request,
-    background_tasks: BackgroundTasks,
-    db: Session = Depends(get_db)
-):
-    """Add a new FAQ Inquiry for both visitors and authenticated users."""
-    current_user = request.state.current_user
-    user_id = current_user.id if current_user else None  # Set user_id only if authenticated
-
-    new_faq_inquiry = faq_inquiries_service.create(db, data, user_id)  # Pass user_id (None for visitors)
-
-    # Send email to admin
-    background_tasks.add_task(
-        send_faq_inquiry_mail, 
-        context={
-            "full_name": new_faq_inquiry.full_name,
-            "email": new_faq_inquiry.email,
-            "message": new_faq_inquiry.message,
-        }
-    )
-
-    response = success_response(
-        message=SUCCESS,
-        data={"id": new_faq_inquiry.id},
-        status_code=status.HTTP_201_CREATED,
-    )
-    return response
+ @faq_inquiries.post(
+     "",
+     response_model=success_response,
+     status_code=status.HTTP_201_CREATED,
+     responses={
+         201: {"description": "FAQ Inquiry created successfully"},
+         422: {"description": "Validation Error"},
+     },
+ )
+ async def create_faq_inquiry(
+     data: CreateFAQInquiry,
+     request: Request,
+     background_tasks: BackgroundTasks,
+     db: Session = Depends(get_db)
+ ):
+     """Add a new FAQ Inquiry for both visitors and authenticated users."""
+     access_token = request.headers.get("Authorization")
+     current_user = user_service.get_current_user_or_none(access_token, db)
+     user_id = current_user.id if current_user else None  # Set user_id only if authenticated
+ 
+     new_faq_inquiry = faq_inquiries_service.create(db, data, user_id)  # Pass user_id (None for visitors)
+ 
+     # Send email to admin
+     background_tasks.add_task(
+         send_faq_inquiry_mail, 
+         context={
+             "full_name": new_faq_inquiry.full_name,
+             "email": new_faq_inquiry.email,
+             "message": new_faq_inquiry.message,
+         }
+     )
+ 
+     response = success_response(
+         message=SUCCESS,
+         data={"id": new_faq_inquiry.id},
+         status_code=status.HTTP_201_CREATED,
+     )
+     return response
 
 # READ
 @faq_inquiries.get(
