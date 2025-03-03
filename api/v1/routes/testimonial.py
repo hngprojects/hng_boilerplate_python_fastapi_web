77f4a05fdc195ab798e37ae927ad9f514d99f6c0
mikemando <ikwuegbu300@gmail.com>
2025-03-03 04:01:17 +0100

    fix: resolved to using paginated_response method to handle endpoint request

--- api/v1/routes/testimonial.py
+++ api/v1/routes/testimonial.py
@@ -5,6 +5,7 @@ Module contains CRUD routes for testimonial
 from fastapi.encoders import jsonable_encoder
 from api.db.database import get_db
 from sqlalchemy.orm import Session
+from sqlalchemy import desc
 from api.v1.models.user import User
 from fastapi import Depends, APIRouter, status,Query
 from api.utils.success_response import success_response, fail_response
@@ -42,23 +43,15 @@ def get_top_rated_testimonials(
 ):
     """Endpoint to fetch top-rated testimonials"""
     try:
+        return paginated_response(
+            db=db,
+            model=Testimonial,
+            skip=(page - 1) * per_page,
+            limit=per_page,
+            filters={},
+            order_by=desc(Testimonial.ratings)
+        )
 
-        testimonials = testimonial_service.top_rated_testimonials(db, page, per_page)
-
-        if not testimonials:
-            return success_response(status_code=200, message="No testimonials found.", data=[])
-    
-        return success_response(
-            status_code=200,
-            message="Top-rated testimonials retrieved successfully.",
-            data=[{
-                "id": testimonial.id,
-                "content": testimonial.content,
-                "ratings": testimonial.ratings,
-                "author_id": testimonial.author_id,
-                "created_at": testimonial.created_at.isoformat()
-            } for testimonial in testimonials]
-    )
     except Exception as e:
         return fail_response(status_code=500, message="An error occurred.", data={"error": str(e)})
 
