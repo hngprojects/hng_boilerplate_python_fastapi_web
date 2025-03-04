f2f8f67ff9d6965400dcddc44f14f8f75e85258a
Dafinci01 <Davehacker382gmail.com>
2025-03-04 15:10:57 +0100

    feat: Pagination seervice layer added

--- api/v1/services/activity_logs.py
+++ api/v1/services/activity_logs.py
@@ -1,39 +1,43 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import desc  
 from api.v1.models.activity_logs import ActivityLog
 from typing import Optional, Any
-
-
 
 class ActivityLogService:
     """Activity Log service"""
 
     def create_activity_log(self, db: Session, user_id: str, action: str):
         """Creates a new activity log"""
-
         activity_log = ActivityLog(user_id=user_id, action=action)
         db.add(activity_log)
         db.commit()
         db.refresh(activity_log)
         return activity_log
 
-    def fetch_all(self, db: Session, **query_params: Optional[Any]):
-        """Fetch all products with option tto search using query parameters"""
-
+    def fetch_all(self, db: Session, page: int, limit: int, user_id: Optional[str] = None, **query_params: Optional[Any]):
+        """Fetch all products with option to search using query parameters"""
+        offset = (page - 1) * limit
         query = db.query(ActivityLog)
 
-        # Enable filter by query parameter
+        if user_id:  # Filter by user id 
+            query = query.filter(ActivityLog.user_id == user_id)
+
+        # Enable filter by arbitrary query parameters
         if query_params:
             for column, value in query_params.items():
                 if hasattr(ActivityLog, column) and value:
                     query = query.filter(
                         getattr(ActivityLog, column).ilike(f"%{value}%")
                     )
-        
-        return query.all()
-    
-    def delete_activity_log_by_id(self, db: Session, log_id: str) -> bool:
+
+       
+        query = query.order_by(desc(ActivityLog.timestamp))
+        paginated_logs = query.offset(offset).limit(limit).all()
+        return paginated_logs 
+
+    def delete_activity_log_by_id(self, db: Session, log_id: str) -> dict:
         """Delete an activity log by ID with error handling"""
         log = db.query(ActivityLog).filter(ActivityLog.id == log_id).first()
 
@@ -47,6 +51,5 @@ class ActivityLogService:
         db.commit()
 
         return {"status": "success", "detail": f"Activity log with ID {log_id} deleted successfully"}
-
 
 activity_log_service = ActivityLogService()
